ba1adbec737482ab5f3d18ad75fc566795634232
KingNarmar <mina.bushra1990@gmail.com>
2026-06-09 12:19:47 +0400

    Fix analyzer warnings in base architectural contracts

--- lib/core/errors/common_failures.dart
+++ lib/core/errors/common_failures.dart
@@ -1,64 +1,40 @@
 import 'failure.dart';
 
 class ValidationFailure extends Failure {
-  const ValidationFailure({
-    required super.message,
-    super.code,
-  });
+  const ValidationFailure({required super.message, super.code});
 }
 
 class NetworkFailure extends Failure {
-  const NetworkFailure({
-    required super.message,
-    super.code,
-  });
+  const NetworkFailure({required super.message, super.code});
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure({
-    required super.message,
-    super.code,
-  });
+  const ServerFailure({required super.message, super.code});
 }
 
 class CacheFailure extends Failure {
-  const CacheFailure({
-    required super.message,
-    super.code,
-  });
+  const CacheFailure({required super.message, super.code});
 }
 
 class AuthFailure extends Failure {
-  const AuthFailure({
-    required super.message,
-    super.code,
-  });
+  const AuthFailure({required super.message, super.code});
 }
 
 class PermissionFailure extends Failure {
-  const PermissionFailure({
-    required super.message,
-    super.code,
-  });
+  const PermissionFailure({required super.message, super.code});
 }
 
 class NotFoundFailure extends Failure {
-  const NotFoundFailure({
-    required super.message,
-    super.code,
-  });
+  const NotFoundFailure({required super.message, super.code});
 }
 
 class ConflictFailure extends Failure {
-  const ConflictFailure({
-    required super.message,
-    super.code,
-  });
+  const ConflictFailure({required super.message, super.code});
 }
 
 class UnexpectedFailure extends Failure {
   const UnexpectedFailure({
-    String message = 'Unexpected error occurred.',
+    super.message = 'Unexpected error occurred.',
     super.code,
-  }) : super(message: message);
+  });
 }
--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,11 +1,11 @@
 import '../utils/result.dart';
 
-abstract class UseCase<Type, Params> {
-  Future<Result<Type>> call(Params params);
+abstract class UseCase<Output, Params> {
+  Future<Result<Output>> call(Params params);
 }
 
-abstract class StreamUseCase<Type, Params> {
-  Stream<Result<Type>> call(Params params);
+abstract class StreamUseCase<Output, Params> {
+  Stream<Result<Output>> call(Params params);
 }
 
 class NoParams {
