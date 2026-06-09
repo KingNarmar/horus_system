e15d3e13a404648d94f27052cf83ce4843f6dc7d
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-09 16:30:59 +0400

    Add get company permissions use case

---
+++ lib/features/company/domain/usecases/get_company_permissions_usecase.dart
@@ -0,0 +1,27 @@
+import '../../../../core/usecases/usecase.dart';
+import '../../../../core/utils/result.dart';
+import '../entities/company_permissions.dart';
+import '../entities/current_company_context.dart';
+import '../policies/company_permission_policy.dart';
+
+class GetCompanyPermissionsParams {
+  final CurrentCompanyContext currentCompanyContext;
+
+  const GetCompanyPermissionsParams({required this.currentCompanyContext});
+}
+
+class GetCompanyPermissionsUseCase
+    implements UseCase<CompanyPermissions, GetCompanyPermissionsParams> {
+  const GetCompanyPermissionsUseCase();
+
+  @override
+  Future<Result<CompanyPermissions>> call(
+    GetCompanyPermissionsParams params,
+  ) async {
+    return Success(
+      CompanyPermissionPolicy.permissionsFor(
+        params.currentCompanyContext.role,
+      ),
+    );
+  }
+}
