5ecad94f20e3010f1de8ce3172d7fb38189c6e19
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-09 16:11:31 +0400

    Wire current company dependencies

--- lib/core/di/app_dependencies.dart
+++ lib/core/di/app_dependencies.dart
@@ -4,14 +4,28 @@ import '../../features/auth/domain/usecases/get_current_user_usecase.dart';
 import '../../features/auth/domain/usecases/login_usecase.dart';
 import '../../features/auth/domain/usecases/logout_usecase.dart';
 import '../../features/auth/presentation/cubit/auth_cubit.dart';
+import '../../features/company/data/datasources/company_context_remote_data_source.dart';
 import '../../features/company/data/datasources/company_remote_data_source.dart';
+import '../../features/company/data/repositories/company_context_repository_impl.dart';
 import '../../features/company/data/repositories/company_repository_impl.dart';
+import '../../features/company/domain/usecases/clear_current_company_context_usecase.dart';
 import '../../features/company/domain/usecases/create_company_usecase.dart';
 import '../../features/company/domain/usecases/get_my_companies_usecase.dart';
+import '../../features/company/domain/usecases/load_current_company_context_usecase.dart';
+import '../../features/company/domain/usecases/select_current_company_usecase.dart';
 import '../../features/company/presentation/cubit/company_onboarding_cubit.dart';
+import '../../features/company/presentation/cubit/current_company_cubit.dart';
+import '../context/current_company_provider.dart';
+import '../context/in_memory_current_company_provider.dart';
 import '../data/supabase/supabase_client_provider.dart';
 
 abstract final class AppDependencies {
+  static final CurrentCompanyProvider _currentCompanyProvider =
+      InMemoryCurrentCompanyProvider();
+
+  static CurrentCompanyProvider get currentCompanyProvider =>
+      _currentCompanyProvider;
+
   static AuthCubit createAuthCubit() {
     final authRemoteDataSource = SupabaseAuthRemoteDataSource(
       SupabaseClientProvider.client,
@@ -34,6 +48,29 @@ abstract final class AppDependencies {
     return CompanyOnboardingCubit(
       createCompanyUseCase: CreateCompanyUseCase(companyRepository),
       getMyCompaniesUseCase: GetMyCompaniesUseCase(companyRepository),
+    );
+  }
+
+  static CurrentCompanyCubit createCurrentCompanyCubit() {
+    final companyContextRemoteDataSource =
+        SupabaseCompanyContextRemoteDataSource(
+      SupabaseClientProvider.client,
+    );
+    final companyContextRepository = CompanyContextRepositoryImpl(
+      remoteDataSource: companyContextRemoteDataSource,
+      currentCompanyProvider: _currentCompanyProvider,
+    );
+
+    return CurrentCompanyCubit(
+      loadCurrentCompanyContextUseCase: LoadCurrentCompanyContextUseCase(
+        companyContextRepository,
+      ),
+      selectCurrentCompanyUseCase: SelectCurrentCompanyUseCase(
+        companyContextRepository,
+      ),
+      clearCurrentCompanyContextUseCase: ClearCurrentCompanyContextUseCase(
+        companyContextRepository,
+      ),
     );
   }
 }
