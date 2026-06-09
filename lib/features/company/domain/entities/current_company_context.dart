8579df5f823b5a9b96cf9c855dcacf482bc101ca
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-09 16:08:26 +0400

    Add current company context entity

---
+++ lib/features/company/domain/entities/current_company_context.dart
@@ -0,0 +1,18 @@
+import 'company.dart';
+import 'company_role.dart';
+
+class CurrentCompanyContext {
+  final Company company;
+  final CompanyRole role;
+
+  const CurrentCompanyContext({
+    required this.company,
+    required this.role,
+  });
+
+  String get companyId => company.id;
+
+  bool get canManageCompany {
+    return role == CompanyRole.owner || role == CompanyRole.admin;
+  }
+}
