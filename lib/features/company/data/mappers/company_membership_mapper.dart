3f5727a77efd1a6bf3cdd29930415307b4f043c1
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-09 16:09:43 +0400

    Add company membership mapper

---
+++ lib/features/company/data/mappers/company_membership_mapper.dart
@@ -0,0 +1,21 @@
+import '../../domain/entities/company_membership.dart';
+import '../../domain/entities/current_company_context.dart';
+import 'company_mapper.dart';
+import '../models/company_membership_model.dart';
+
+extension CompanyMembershipModelMapper on CompanyMembershipModel {
+  CompanyMembership toEntity() {
+    return CompanyMembership(
+      company: company.toEntity(),
+      role: role,
+      isActive: isActive,
+    );
+  }
+
+  CurrentCompanyContext toCurrentCompanyContext() {
+    return CurrentCompanyContext(
+      company: company.toEntity(),
+      role: role,
+    );
+  }
+}
