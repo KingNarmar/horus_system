3fa5ac971402dbe131237fe7b1ca8e761bd7d7ac
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-09 16:11:19 +0400

    Register current company cubit

--- lib/app.dart
+++ lib/app.dart
@@ -10,6 +10,7 @@ import 'core/theme/app_theme.dart';
 import 'features/auth/presentation/cubit/auth_cubit.dart';
 import 'features/auth/presentation/pages/auth_gate.dart';
 import 'features/company/presentation/cubit/company_onboarding_cubit.dart';
+import 'features/company/presentation/cubit/current_company_cubit.dart';
 import 'l10n/app_localizations.dart';
 
 class HorusApp extends StatelessWidget {
@@ -21,6 +22,9 @@ class HorusApp extends StatelessWidget {
       providers: [
         BlocProvider<AuthCubit>(
           create: (_) => AppDependencies.createAuthCubit()..checkCurrentUser(),
+        ),
+        BlocProvider<CurrentCompanyCubit>(
+          create: (_) => AppDependencies.createCurrentCompanyCubit(),
         ),
         BlocProvider<CompanyOnboardingCubit>(
           create: (_) => AppDependencies.createCompanyOnboardingCubit(),
