cfde27ca454cd930afa83bcb97d3405c0762e684
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-09 14:56:37 +0400

    Implement auth user mapper

--- lib/features/auth/data/mappers/auth_user_mapper.dart
+++ lib/features/auth/data/mappers/auth_user_mapper.dart
@@ -1,1 +1,14 @@
-// Auth user mapper will be implemented in the next commit.
+import '../../domain/entities/auth_user.dart';
+import '../models/auth_user_model.dart';
+
+extension AuthUserModelMapper on AuthUserModel {
+  AuthUser toEntity() {
+    return AuthUser(
+      id: id,
+      email: email,
+      phone: phone,
+      fullName: fullName,
+      isEmailConfirmed: isEmailConfirmed,
+    );
+  }
+}
