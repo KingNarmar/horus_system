fa852e6fb693a287beb494279e19bdc92e9a94d1
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-09 15:43:46 +0400

    Wire company onboarding dependencies

--- lib/core/di/app_dependencies.dart
+++ lib/core/di/app_dependencies.dart
@@ -4,6 +4,11 @@ import '../../features/auth/domain/usecases/get_current_user_usecase.dart';
 import '../../features/auth/domain/usecases/login_usecase.dart';
 import '../../features/auth/domain/usecases/logout_usecase.dart';
 import '../../features/auth/presentation/cubit/auth_cubit.dart';
+import '../../features/company/data/datasources/company_remote_data_source.dart';
+import '../../features/company/data/repositories/company_repository_impl.dart';
+import '../../features/company/domain/usecases/create_company_usecase.dart';
+import '../../features/company/domain/usecases/get_my_companies_usecase.dart';
+import '../../features/company/presentation/cubit/company_onboarding_cubit.dart';
 import '../data/supabase/supabase_client_provider.dart';
 
 abstract final class AppDependencies {
@@ -17,6 +22,18 @@ abstract final class AppDependencies {
       loginUseCase: LoginUseCase(authRepository),
       logoutUseCase: LogoutUseCase(authRepository),
       getCurrentUserUseCase: GetCurrentUserUseCase(authRepository),
+    );
+  }
+
+  static CompanyOnboardingCubit createCompanyOnboardingCubit() {
+    final companyRemoteDataSource = SupabaseCompanyRemoteDataSource(
+      SupabaseClientProvider.client,
+    );
+    final companyRepository = CompanyRepositoryImpl(companyRemoteDataSource);
+
+    return CompanyOnboardingCubit(
+      createCompanyUseCase: CreateCompanyUseCase(companyRepository),
+      getMyCompaniesUseCase: GetMyCompaniesUseCase(companyRepository),
     );
   }
 }
