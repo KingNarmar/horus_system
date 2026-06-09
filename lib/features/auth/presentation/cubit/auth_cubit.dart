0b79ee2d7323c81055436a907fb6ccdc2b734aea
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-09 15:03:22 +0400

    Add auth cubit

---
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/usecases/usecase.dart';
+import '../../domain/usecases/get_current_user_usecase.dart';
+import '../../domain/usecases/login_usecase.dart';
+import '../../domain/usecases/logout_usecase.dart';
+import 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  final LoginUseCase _loginUseCase;
+  final LogoutUseCase _logoutUseCase;
+  final GetCurrentUserUseCase _getCurrentUserUseCase;
+
+  AuthCubit({
+    required LoginUseCase loginUseCase,
+    required LogoutUseCase logoutUseCase,
+    required GetCurrentUserUseCase getCurrentUserUseCase,
+  })  : _loginUseCase = loginUseCase,
+        _logoutUseCase = logoutUseCase,
+        _getCurrentUserUseCase = getCurrentUserUseCase,
+        super(const AuthInitial());
+
+  Future<void> checkCurrentUser() async {
+    emit(const AuthLoading());
+
+    final result = await _getCurrentUserUseCase(const NoParams());
+
+    result.when(
+      success: (user) {
+        if (user == null) {
+          emit(const AuthUnauthenticated());
+          return;
+        }
+
+        emit(AuthAuthenticated(user));
+      },
+      failure: (failure) => emit(AuthFailureState(failure)),
+    );
+  }
+
+  Future<void> login({
+    required String email,
+    required String password,
+  }) async {
+    emit(const AuthLoading());
+
+    final result = await _loginUseCase(
+      LoginParams(email: email, password: password),
+    );
+
+    result.when(
+      success: (user) => emit(AuthAuthenticated(user)),
+      failure: (failure) => emit(AuthFailureState(failure)),
+    );
+  }
+
+  Future<void> logout() async {
+    emit(const AuthLoading());
+
+    final result = await _logoutUseCase(const NoParams());
+
+    result.when(
+      success: (_) => emit(const AuthUnauthenticated()),
+      failure: (failure) => emit(AuthFailureState(failure)),
+    );
+  }
+}
