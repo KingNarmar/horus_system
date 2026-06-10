272c270cdf9aea3a060cc9197d37c12566617c15
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-10 15:25:30 +0400

    Add customers domain repository contract

---
+++ lib/features/customers/domain/repositories/customers_repository.dart
@@ -0,0 +1,19 @@
+import '../../../../core/utils/result.dart';
+import '../entities/customer.dart';
+import '../entities/customer_write_data.dart';
+
+abstract class CustomersRepository {
+  Future<Result<List<Customer>>> getCustomers({required String companyId});
+
+  Future<Result<Customer>> addCustomer({required CustomerWriteData data});
+
+  Future<Result<Customer>> updateCustomer({
+    required String customerId,
+    required CustomerWriteData data,
+  });
+
+  Future<Result<Customer>> deactivateCustomer({
+    required String companyId,
+    required String customerId,
+  });
+}
