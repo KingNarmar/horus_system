d640f57c4dad0c7aa43d4260d3a2998679fd49f4
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-10 13:15:21 +0400

    Wire Device Preview into MaterialApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -38,6 +39,8 @@ class HorusApp extends StatelessWidget {
         onGenerateTitle: (context) => context.l10n.appTitle,
         debugShowCheckedModeBanner: false,
         theme: AppTheme.light,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
         home: const AuthGate(),
