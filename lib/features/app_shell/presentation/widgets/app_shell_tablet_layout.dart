775e605f93a2a4e04845d11b20ff9ba111c46517
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-10 13:43:41 +0400

    Localize tablet app shell layout

--- lib/features/app_shell/presentation/widgets/app_shell_tablet_layout.dart
+++ lib/features/app_shell/presentation/widgets/app_shell_tablet_layout.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/constants/app_spacing.dart';
+import '../../../../core/localization/app_localizations_extension.dart';
+import '../../../../core/localization/widgets/app_language_toggle_button.dart';
 import '../../../company/domain/entities/current_company_context.dart';
 import '../models/app_shell_destination.dart';
 import 'app_shell_body.dart';
@@ -22,6 +25,8 @@ class AppShellTabletLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
     return Scaffold(
       body: SafeArea(
         child: Row(
@@ -30,11 +35,15 @@ class AppShellTabletLayout extends StatelessWidget {
               selectedIndex: selectedIndex,
               onDestinationSelected: onSelect,
               labelType: NavigationRailLabelType.all,
+              leading: const Padding(
+                padding: EdgeInsets.only(bottom: AppSpacing.md),
+                child: AppLanguageToggleButton.compact(),
+              ),
               trailing: Expanded(
                 child: Align(
                   alignment: Alignment.bottomCenter,
                   child: IconButton(
-                    tooltip: 'Logout',
+                    tooltip: l10n.logout,
                     onPressed: onLogout,
                     icon: const Icon(Icons.logout_outlined),
                   ),
@@ -45,7 +54,7 @@ class AppShellTabletLayout extends StatelessWidget {
                     (item) => NavigationRailDestination(
                       icon: Icon(item.icon),
                       selectedIcon: Icon(item.selectedIcon),
-                      label: Text(item.label),
+                      label: Text(item.label(context)),
                     ),
                   )
                   .toList(),
