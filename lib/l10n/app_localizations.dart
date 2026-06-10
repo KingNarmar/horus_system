a4835b890f30086eef00bf3fea15231916cf9900
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-10 13:45:03 +0400

    Regenerate localization base API

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -10,57 +10,6 @@ import 'app_localizations_en.dart';
 
 // ignore_for_file: type=lint
 
-/// Callers can lookup localized strings with an instance of AppLocalizations
-/// returned by `AppLocalizations.of(context)`.
-///
-/// Applications need to include `AppLocalizations.delegate()` in their app's
-/// `localizationDelegates` list, and the locales they support in the app's
-/// `supportedLocales` list. For example:
-///
-/// ```dart
-/// import 'l10n/app_localizations.dart';
-///
-/// return MaterialApp(
-///   localizationsDelegates: AppLocalizations.localizationsDelegates,
-///   supportedLocales: AppLocalizations.supportedLocales,
-///   home: MyApplicationHome(),
-/// );
-/// ```
-///
-/// ## Update pubspec.yaml
-///
-/// Please make sure to update your pubspec.yaml to include the following
-/// packages:
-///
-/// ```yaml
-/// dependencies:
-///   # Internationalization support.
-///   flutter_localizations:
-///     sdk: flutter
-///   intl: any # Use the pinned version from flutter_localizations
-///
-///   # Rest of dependencies
-/// ```
-///
-/// ## iOS Applications
-///
-/// iOS applications define key application metadata, including supported
-/// locales, in an Info.plist file that is built into the application bundle.
-/// To configure the locales supported by your app, you’ll need to edit this
-/// file.
-///
-/// First, open your project’s ios/Runner.xcworkspace Xcode workspace file.
-/// Then, in the Project Navigator, open the Info.plist file under the Runner
-/// project’s Runner folder.
-///
-/// Next, select the Information Property List item, select Add Item from the
-/// Editor menu, then select Localizations from the pop-up menu.
-///
-/// Select and expand the newly-created Localizations item then, for each
-/// locale your application supports, add a new item and select the locale
-/// you wish to add from the pop-up menu in the Value field. This list should
-/// be consistent with the languages listed in the AppLocalizations.supportedLocales
-/// property.
 abstract class AppLocalizations {
   AppLocalizations(String locale)
     : localeName = intl.Intl.canonicalizedLocale(locale.toString());
@@ -74,16 +23,6 @@ abstract class AppLocalizations {
   static const LocalizationsDelegate<AppLocalizations> delegate =
       _AppLocalizationsDelegate();
 
-  /// A list of this localizations delegate along with the default localizations
-  /// delegates.
-  ///
-  /// Returns a list of localizations delegates containing this delegate along with
-  /// GlobalMaterialLocalizations.delegate, GlobalCupertinoLocalizations.delegate,
-  /// and GlobalWidgetsLocalizations.delegate.
-  ///
-  /// Additional delegates can be added by appending to this list in
-  /// MaterialApp. This list does not have to be used at all if a custom list
-  /// of delegates is preferred or required.
   static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
       <LocalizationsDelegate<dynamic>>[
         delegate,
@@ -92,35 +31,53 @@ abstract class AppLocalizations {
         GlobalWidgetsLocalizations.delegate,
       ];
 
-  /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
     Locale('ar'),
     Locale('en'),
   ];
 
-  /// The application title.
-  ///
-  /// In en, this message translates to:
-  /// **'H.O.R.U.S System'**
   String get appTitle;
-
-  /// The application subtitle.
-  ///
-  /// In en, this message translates to:
-  /// **'Heavy Operations & Route Unified System'**
   String get appSubtitle;
-
-  /// Short description displayed on the launch page.
-  ///
-  /// In en, this message translates to:
-  /// **'SaaS platform for heavy transport operations.'**
   String get launchDescription;
-
-  /// Architecture rule badge displayed on the launch page.
-  ///
-  /// In en, this message translates to:
-  /// **'Clean Architecture by the book • SOLID Principles'**
   String get architectureBadge;
+
+  String get appShellDashboardLabel;
+  String get appShellDashboardDescription;
+  String get appShellCustomersLabel;
+  String get appShellCustomersDescription;
+  String get appShellDriversLabel;
+  String get appShellDriversDescription;
+  String get appShellFleetLabel;
+  String get appShellFleetDescription;
+  String get appShellRoutesLabel;
+  String get appShellRoutesDescription;
+  String get appShellTripsLabel;
+  String get appShellTripsDescription;
+  String get appShellExpensesLabel;
+  String get appShellExpensesDescription;
+  String get appShellInvoicesLabel;
+  String get appShellInvoicesDescription;
+  String get appShellReportsLabel;
+  String get appShellReportsDescription;
+  String get appShellSettingsLabel;
+  String get appShellSettingsDescription;
+  String get appShellMoreLabel;
+
+  String get logout;
+  String companyWithName(String companyName);
+  String roleWithName(String roleName);
+  String get companySettingsTitle;
+  String get manageUsers;
+  String get noPermissionManageUsers;
+  String get switchToArabic;
+  String get switchToEnglish;
+
+  String get roleOwner;
+  String get roleAdmin;
+  String get roleOperations;
+  String get roleAccountant;
+  String get roleViewer;
+  String get roleDriver;
 }
 
 class _AppLocalizationsDelegate
@@ -141,7 +98,6 @@ class _AppLocalizationsDelegate
 }
 
 AppLocalizations lookupAppLocalizations(Locale locale) {
-  // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
     case 'ar':
       return AppLocalizationsAr();
