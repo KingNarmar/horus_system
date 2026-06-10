eee6c20fba875842ad1b0b80b7e5d2f17c04b084
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-10 11:36:18 +0400

    Add register contract to auth repository

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,6 +2,13 @@ import '../../../../core/utils/result.dart';
 import '../entities/auth_user.dart';
 
 abstract class AuthRepository {
+  Future<Result<AuthUser>> register({
+    required String fullName,
+    required String phone,
+    required String email,
+    required String password,
+  });
+
   Future<Result<AuthUser>> login({
     required String email,
     required String password,
