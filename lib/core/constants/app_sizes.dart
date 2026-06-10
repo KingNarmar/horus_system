d32cc5d232eb798c457ecc8a3cc97e4febc653d1
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-10 16:19:19 +0400

    Increase desktop content width for data tables

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -4,11 +4,11 @@ abstract final class AppSizes {
 
   static const double desktopSidebarWidth = 280;
   static const double tabletRailWidth = 80;
-  static const double desktopMinWidth = 720;
+  static const double desktopMinWidth = 900;
 
   static const double mobileMaxContentWidth = 420;
   static const double tabletMaxContentWidth = 620;
-  static const double desktopMaxContentWidth = 720;
+  static const double desktopMaxContentWidth = 1200;
 
   static const double iconSm = 18;
   static const double iconMd = 24;
