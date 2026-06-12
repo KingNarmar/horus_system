69793464b9cecebbe12d494621ca78cf61990d06
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-12 11:35:18 +0400

    Add audit action domain entity

---
+++ lib/features/audit/domain/entities/audit_action.dart
@@ -0,0 +1,19 @@
+enum AuditAction {
+  created,
+  updated,
+  deactivated,
+  reactivated,
+  statusChanged,
+}
+
+extension AuditActionX on AuditAction {
+  String get value {
+    return switch (this) {
+      AuditAction.created => 'created',
+      AuditAction.updated => 'updated',
+      AuditAction.deactivated => 'deactivated',
+      AuditAction.reactivated => 'reactivated',
+      AuditAction.statusChanged => 'status_changed',
+    };
+  }
+}
