adb7412c8f15e0709ddec555af566ddeb0701f9f
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-12 12:30:42 +0400

    Add audit log read repository contract

--- lib/features/audit/domain/repositories/audit_log_repository.dart
+++ lib/features/audit/domain/repositories/audit_log_repository.dart
@@ -1,6 +1,16 @@
 import '../../../../core/utils/result.dart';
+import '../entities/audit_entity_type.dart';
+import '../entities/audit_log.dart';
 import '../entities/audit_log_write_data.dart';
+import '../entities/audit_module.dart';
 
 abstract class AuditLogRepository {
   Future<Result<void>> createAuditLog({required AuditLogWriteData data});
+
+  Future<Result<List<AuditLog>>> getEntityAuditLogs({
+    required String companyId,
+    required AuditModule module,
+    required AuditEntityType entityType,
+    required String entityId,
+  });
 }
