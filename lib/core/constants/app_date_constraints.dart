c1c67cd75e70f3d20b0e4062430160c4d376b2ba
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-12 14:40:17 +0400

    Add shared app date constraints

---
+++ lib/core/constants/app_date_constraints.dart
@@ -0,0 +1,6 @@
+abstract final class AppDateConstraints {
+  const AppDateConstraints._();
+
+  static const int driverLicenseExpiryPastYears = 0;
+  static const int driverLicenseExpiryFutureYears = 30;
+}
