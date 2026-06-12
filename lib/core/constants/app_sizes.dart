d5594bf0cef175e821074cb4ea77608d0ef8b24d
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-12 14:47:43 +0400

    Add shared form and loading sizes

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -8,9 +8,13 @@ abstract final class AppSizes {
 
   static const double mobileMaxContentWidth = 420;
   static const double tabletMaxContentWidth = 620;
+  static const double desktopAuthFormMaxWidth = 520;
   static const double desktopMaxContentWidth = 1200;
 
   static const double iconSm = 18;
   static const double iconMd = 24;
   static const double iconLg = 32;
+
+  static const double loadingIndicatorSm = 20;
+  static const double loadingIndicatorStrokeWidth = 2;
 }
