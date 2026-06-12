af498a69add30b1a8012babbfe68c310eb3267ae
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-12 11:37:57 +0400

    Extend customers repository for audit actor and reactivation

--- lib/features/customers/domain/repositories/customers_repository.dart
+++ lib/features/customers/domain/repositories/customers_repository.dart
@@ -5,15 +5,26 @@ import '../entities/customer_write_data.dart';
 abstract class CustomersRepository {
   Future<Result<List<Customer>>> getCustomers({required String companyId});
 
-  Future<Result<Customer>> addCustomer({required CustomerWriteData data});
+  Future<Result<Customer>> addCustomer({
+    required CustomerWriteData data,
+    required String actorRole,
+  });
 
   Future<Result<Customer>> updateCustomer({
     required String customerId,
     required CustomerWriteData data,
+    required String actorRole,
   });
 
   Future<Result<Customer>> deactivateCustomer({
     required String companyId,
     required String customerId,
+    required String actorRole,
+  });
+
+  Future<Result<Customer>> reactivateCustomer({
+    required String companyId,
+    required String customerId,
+    required String actorRole,
   });
 }
