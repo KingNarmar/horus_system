498d24607b15c40f822b65d99f4fea3bb25e5c1e
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-19 12:12:19 +0400

    Add fleet repo implementation

---
+++ lib/features/fleet/data/repositories/fleet_repo_impl.dart
@@ -0,0 +1,25 @@
+import '../../../../core/utils/result.dart';
+import '../../domain/entities/tractor_head.dart';
+import '../../domain/entities/trailer_entity.dart';
+import '../../domain/repositories/fleet_repository.dart';
+import '../datasources/fleet_remote_data_source.dart';
+import '../mappers/tractor_mapper.dart';
+import '../mappers/trailers_mapper.dart';
+
+class FleetRepositoryImpl implements FleetRepository {
+  final FleetRemoteDataSource remoteDataSource;
+
+  const FleetRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<Result<List<TractorHead>>> getTractorHeads({required String companyId}) async {
+    final models = await remoteDataSource.getTractorHeads(companyId: companyId);
+    return Success(models.map((model) => model.toEntity()).toList());
+  }
+
+  @override
+  Future<Result<List<TrailerEntity>>> getTrailers({required String companyId}) async {
+    final models = await remoteDataSource.getTrailers(companyId: companyId);
+    return Success(models.map((model) => model.toEntity()).toList());
+  }
+}
