ab2125ccbf3ce47020de42fae8e1a87b072d643d
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-19 13:24:26 +0400

    Add fleet deactivate reactivate use cases

--- lib/features/fleet/domain/usecases/fleet_usecases.dart
+++ lib/features/fleet/domain/usecases/fleet_usecases.dart
@@ -55,8 +55,14 @@ class SaveTrailerParams {
   });
 }
 
-class GetTractorHeadsUseCase
-    implements UseCase<List<TractorHead>, GetFleetParams> {
+class FleetAssetStatusParams {
+  final CurrentCompanyContext currentCompanyContext;
+  final String id;
+
+  const FleetAssetStatusParams({required this.currentCompanyContext, required this.id});
+}
+
+class GetTractorHeadsUseCase implements UseCase<List<TractorHead>, GetFleetParams> {
   final FleetRepository _repository;
 
   const GetTractorHeadsUseCase(this._repository);
@@ -78,8 +84,7 @@ class GetTractorHeadsUseCase
   }
 }
 
-class GetTrailersUseCase
-    implements UseCase<List<TrailerEntity>, GetFleetParams> {
+class GetTrailersUseCase implements UseCase<List<TrailerEntity>, GetFleetParams> {
   final FleetRepository _repository;
 
   const GetTrailersUseCase(this._repository);
@@ -101,8 +106,7 @@ class GetTrailersUseCase
   }
 }
 
-class SaveTractorHeadUseCase
-    implements UseCase<TractorHead, SaveTractorHeadParams> {
+class SaveTractorHeadUseCase implements UseCase<TractorHead, SaveTractorHeadParams> {
   final FleetRepository _repository;
 
   const SaveTractorHeadUseCase(this._repository);
@@ -143,16 +147,9 @@ class SaveTractorHeadUseCase
 
     final id = _normalizeOptional(params.id);
     if (id == null) {
-      return _repository.addTractorHead(
-        data: data,
-        actorRole: context.role.value,
-      );
+      return _repository.addTractorHead(data: data, actorRole: context.role.value);
     }
-    return _repository.saveTractorHead(
-      id: id,
-      data: data,
-      actorRole: context.role.value,
-    );
+    return _repository.saveTractorHead(id: id, data: data, actorRole: context.role.value);
   }
 }
 
@@ -199,12 +196,82 @@ class SaveTrailerUseCase implements UseCase<TrailerEntity, SaveTrailerParams> {
     if (id == null) {
       return _repository.addTrailer(data: data, actorRole: context.role.value);
     }
-    return _repository.editTrailer(
-      id: id,
-      data: data,
-      actorRole: context.role.value,
+    return _repository.editTrailer(id: id, data: data, actorRole: context.role.value);
+  }
+}
+
+class DeactivateTractorHeadUseCase implements UseCase<TractorHead, FleetAssetStatusParams> {
+  final FleetRepository _repository;
+
+  const DeactivateTractorHeadUseCase(this._repository);
+
+  @override
+  Future<Result<TractorHead>> call(FleetAssetStatusParams params) {
+    return _changeTractorHeadStatus(params, _repository.deactivateTractorHead);
+  }
+}
+
+class ReactivateTractorHeadUseCase implements UseCase<TractorHead, FleetAssetStatusParams> {
+  final FleetRepository _repository;
+
+  const ReactivateTractorHeadUseCase(this._repository);
+
+  @override
+  Future<Result<TractorHead>> call(FleetAssetStatusParams params) {
+    return _changeTractorHeadStatus(params, _repository.reactivateTractorHead);
+  }
+}
+
+class DeactivateTrailerUseCase implements UseCase<TrailerEntity, FleetAssetStatusParams> {
+  final FleetRepository _repository;
+
+  const DeactivateTrailerUseCase(this._repository);
+
+  @override
+  Future<Result<TrailerEntity>> call(FleetAssetStatusParams params) {
+    return _changeTrailerStatus(params, _repository.deactivateTrailer);
+  }
+}
+
+class ReactivateTrailerUseCase implements UseCase<TrailerEntity, FleetAssetStatusParams> {
+  final FleetRepository _repository;
+
+  const ReactivateTrailerUseCase(this._repository);
+
+  @override
+  Future<Result<TrailerEntity>> call(FleetAssetStatusParams params) {
+    return _changeTrailerStatus(params, _repository.reactivateTrailer);
+  }
+}
+
+Future<Result<TractorHead>> _changeTractorHeadStatus(
+  FleetAssetStatusParams params,
+  Future<Result<TractorHead>> Function({required String companyId, required String id, required String actorRole}) action,
+) {
+  final context = params.currentCompanyContext;
+  if (!FleetPermissionPolicy.canManageFleet(context.role)) {
+    return Future.value(
+      const FailureResult<TractorHead>(
+        PermissionFailure(code: FailureCodes.permissionFleetManagement, message: 'Fleet management is not allowed.'),
+      ),
     );
   }
+  return action(companyId: context.companyId, id: params.id, actorRole: context.role.value);
+}
+
+Future<Result<TrailerEntity>> _changeTrailerStatus(
+  FleetAssetStatusParams params,
+  Future<Result<TrailerEntity>> Function({required String companyId, required String id, required String actorRole}) action,
+) {
+  final context = params.currentCompanyContext;
+  if (!FleetPermissionPolicy.canManageFleet(context.role)) {
+    return Future.value(
+      const FailureResult<TrailerEntity>(
+        PermissionFailure(code: FailureCodes.permissionFleetManagement, message: 'Fleet management is not allowed.'),
+      ),
+    );
+  }
+  return action(companyId: context.companyId, id: params.id, actorRole: context.role.value);
 }
 
 String? _normalizeOptional(String? value) {
