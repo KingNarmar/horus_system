e22d7ff7a8c7f45cc329402f76f2450d8dbcda74
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-19 14:18:50 +0400

    Wire fleet audit activity dependency

--- lib/core/di/fleet_dependencies.dart
+++ lib/core/di/fleet_dependencies.dart
@@ -1,6 +1,7 @@
 import '../../features/audit/data/datasources/audit_logs_remote_data_source.dart';
 import '../../features/audit/data/repositories/audit_log_repository_impl.dart';
 import '../../features/audit/domain/usecases/create_audit_log_usecase.dart';
+import '../../features/audit/domain/usecases/get_entity_audit_logs_usecase.dart';
 import '../../features/fleet/data/datasources/fleet_remote_data_source.dart';
 import '../../features/fleet/data/repositories/fleet_repo_impl.dart';
 import '../../features/fleet/domain/usecases/fleet_usecases.dart';
@@ -26,6 +27,7 @@ abstract final class FleetDependencies {
       reactivateTractorHeadUseCase: ReactivateTractorHeadUseCase(repository),
       deactivateTrailerUseCase: DeactivateTrailerUseCase(repository),
       reactivateTrailerUseCase: ReactivateTrailerUseCase(repository),
+      getEntityAuditLogsUseCase: GetEntityAuditLogsUseCase(auditRepository),
     );
   }
 }
