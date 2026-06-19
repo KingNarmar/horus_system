ca40cc95c058cbcd24aff56d56a67a5ef1b397a3
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-19 12:03:04 +0400

    Add trailer creation contract

--- lib/features/fleet/domain/repositories/fleet_repository.dart
+++ lib/features/fleet/domain/repositories/fleet_repository.dart
@@ -2,6 +2,7 @@ import '../../../../core/utils/result.dart';
 import '../entities/tractor_head.dart';
 import '../entities/tractor_head_write_data.dart';
 import '../entities/trailer_entity.dart';
+import '../entities/trailer_write_data.dart';
 
 abstract class FleetRepository {
   Future<Result<List<TractorHead>>> getTractorHeads({required String companyId});
@@ -10,6 +11,11 @@ abstract class FleetRepository {
 
   Future<Result<TractorHead>> addTractorHead({
     required TractorHeadWriteData data,
+    required String actorRole,
+  });
+
+  Future<Result<TrailerEntity>> addTrailer({
+    required TrailerWriteData data,
     required String actorRole,
   });
 }
