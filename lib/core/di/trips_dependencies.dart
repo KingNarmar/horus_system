9c3c50b0f8d5ee6ffd3a50bc2e64383893ddfec1
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-26 12:39:18 +0400

    feat: wire trip expense dependencies

--- lib/core/di/trips_dependencies.dart
+++ lib/core/di/trips_dependencies.dart
@@ -2,6 +2,9 @@ import '../../features/audit/data/datasources/audit_logs_remote_data_source.dart
 import '../../features/audit/data/repositories/audit_log_repository_impl.dart';
 import '../../features/audit/domain/usecases/create_audit_log_usecase.dart';
 import '../../features/audit/domain/usecases/get_entity_audit_logs_usecase.dart';
+import '../../features/expenses/data/datasources/trip_expenses_remote_data_source.dart';
+import '../../features/expenses/data/repositories/trip_expense_repo_impl.dart';
+import '../../features/expenses/domain/usecases/trip_expenses_usecases.dart';
 import '../../features/trips/data/datasources/trips_remote_data_source.dart';
 import '../../features/trips/data/repositories/trips_repository_impl.dart';
 import '../../features/trips/domain/usecases/trips_usecases.dart';
@@ -16,11 +19,19 @@ abstract final class TripsDependencies {
     final auditRepository = AuditLogRepositoryImpl(
       remoteDataSource: auditRemoteDataSource,
     );
+    final createAuditLogUseCase = CreateAuditLogUseCase(auditRepository);
 
     final tripsRemoteDataSource = SupabaseTripsRemoteDataSource(client);
     final tripsRepository = TripsRepositoryImpl(
       remoteDataSource: tripsRemoteDataSource,
-      createAuditLogUseCase: CreateAuditLogUseCase(auditRepository),
+      createAuditLogUseCase: createAuditLogUseCase,
+    );
+
+    final expensesRemoteDataSource = SupabaseTripExpensesRemoteDataSource(
+      client,
+    );
+    final expensesRepository = TripExpensesRepositoryImpl(
+      remoteDataSource: expensesRemoteDataSource,
     );
 
     return TripsCubit(
@@ -33,6 +44,10 @@ abstract final class TripsDependencies {
       getTripStatusHistoryUseCase: GetTripStatusHistoryUseCase(tripsRepository),
       calculateTripNetProfitUseCase: const CalculateTripNetProfitUseCase(),
       getTripAuditLogsUseCase: GetEntityAuditLogsUseCase(auditRepository),
+      getTripExpensesUseCase: GetTripExpensesUseCase(expensesRepository),
+      getExpenseTypesUseCase: GetExpenseTypesUseCase(expensesRepository),
+      addTripExpenseUseCase: AddTripExpenseUseCase(expensesRepository),
+      updateTripExpenseUseCase: UpdateTripExpenseUseCase(expensesRepository),
     );
   }
 }
