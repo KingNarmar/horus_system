c1fcbf97c074f3fe30bea2f27e721a7d2f864262
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-26 12:33:39 +0400

    feat: add trip expense paid by enum

---
+++ lib/features/expenses/domain/entities/trip_expense_paid_by.dart
@@ -0,0 +1,29 @@
+enum TripExpensePaidBy {
+  company,
+  driverAdvance,
+  driverCash,
+  customer,
+  other,
+}
+
+extension TripExpensePaidByX on TripExpensePaidBy {
+  String get value {
+    return switch (this) {
+      TripExpensePaidBy.company => 'company',
+      TripExpensePaidBy.driverAdvance => 'driver_advance',
+      TripExpensePaidBy.driverCash => 'driver_cash',
+      TripExpensePaidBy.customer => 'customer',
+      TripExpensePaidBy.other => 'other',
+    };
+  }
+
+  static TripExpensePaidBy fromValue(String? value) {
+    return switch (value) {
+      'driver_advance' => TripExpensePaidBy.driverAdvance,
+      'driver_cash' => TripExpensePaidBy.driverCash,
+      'customer' => TripExpensePaidBy.customer,
+      'other' => TripExpensePaidBy.other,
+      _ => TripExpensePaidBy.company,
+    };
+  }
+}
