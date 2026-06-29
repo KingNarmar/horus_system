cec86abf6f510e09f88ff912fc967953bd9346b2
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-29 17:38:28 +0400

    Company expenses UI - Make cubit DI self contained

--- lib/features/company_expenses/di/company_expenses_dependencies.dart
+++ lib/features/company_expenses/di/company_expenses_dependencies.dart
@@ -1,4 +1,7 @@
 import '../../../core/data/supabase/supabase_client_provider.dart';
+import '../../audit/data/datasources/audit_logs_remote_data_source.dart';
+import '../../audit/data/repositories/audit_log_repository_impl.dart';
+import '../../audit/domain/repositories/audit_log_repository.dart';
 import '../../audit/domain/usecases/create_audit_log_usecase.dart';
 import '../data/datasources/company_expenses_remote_data_source.dart';
 import '../data/repositories/company_expenses_repository_impl.dart';
@@ -7,6 +10,24 @@ import '../domain/usecases/company_expenses_usecases.dart';
 import '../presentation/cubit/company_expenses_cubit.dart';
 
 abstract final class CompanyExpensesDependencies {
+  static AuditLogsRemoteDataSource? _auditLogsRemoteDataSourceInstance;
+  static AuditLogsRemoteDataSource get _auditLogsRemoteDataSource =>
+      _auditLogsRemoteDataSourceInstance ??= SupabaseAuditLogsRemoteDataSource(
+        SupabaseClientProvider.client,
+      );
+
+  static AuditLogRepository? _auditLogRepositoryInstance;
+  static AuditLogRepository get _auditLogRepository =>
+      _auditLogRepositoryInstance ??= AuditLogRepositoryImpl(
+        remoteDataSource: _auditLogsRemoteDataSource,
+      );
+
+  static CreateAuditLogUseCase? _createAuditLogUseCaseInstance;
+  static CreateAuditLogUseCase get _createAuditLogUseCase =>
+      _createAuditLogUseCaseInstance ??= CreateAuditLogUseCase(
+        _auditLogRepository,
+      );
+
   static CompanyExpensesRemoteDataSource createRemoteDataSource() {
     return SupabaseCompanyExpensesRemoteDataSource(
       SupabaseClientProvider.client,
@@ -14,20 +35,16 @@ abstract final class CompanyExpensesDependencies {
   }
 
   static CompanyExpensesRepository createRepository({
-    required CreateAuditLogUseCase createAuditLogUseCase,
+    CreateAuditLogUseCase? createAuditLogUseCase,
   }) {
     return CompanyExpensesRepositoryImpl(
       remoteDataSource: createRemoteDataSource(),
-      createAuditLogUseCase: createAuditLogUseCase,
+      createAuditLogUseCase: createAuditLogUseCase ?? _createAuditLogUseCase,
     );
   }
 
-  static CompanyExpensesCubit createCubit({
-    required CreateAuditLogUseCase createAuditLogUseCase,
-  }) {
-    final repository = createRepository(
-      createAuditLogUseCase: createAuditLogUseCase,
-    );
+  static CompanyExpensesCubit createCubit() {
+    final repository = createRepository();
 
     return CompanyExpensesCubit(
       getCategoriesUseCase: createGetCategoriesUseCase(repository),
