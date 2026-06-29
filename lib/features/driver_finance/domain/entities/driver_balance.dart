50dff07421f7610554325c6fd45b0e2a2f5be4dd
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-29 10:15:06 +0400

    Issue 24 add driver balance entity

---
+++ lib/features/driver_finance/domain/entities/driver_balance.dart
@@ -0,0 +1,11 @@
+class DriverBalance {
+  final String companyId;
+  final String driverId;
+  final double totalAdvances;
+
+  const DriverBalance({
+    required this.companyId,
+    required this.driverId,
+    required this.totalAdvances,
+  });
+}
