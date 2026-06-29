a19ad60ef379ddbc36d34c5657b690916195b271
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-29 10:26:11 +0400

    Issue 24 add driver finance actions to drivers page

--- lib/features/drivers/presentation/pages/drivers_page.dart
+++ lib/features/drivers/presentation/pages/drivers_page.dart
@@ -6,6 +6,8 @@ import '../../../../core/constants/app_sizes.dart';
 import '../../../../core/constants/app_spacing.dart';
 import '../../../../core/localization/app_localizations_extension.dart';
 import '../../../company/domain/entities/current_company_context.dart';
+import '../../../driver_finance/domain/entities/driver_financial_movement_type.dart';
+import '../../../driver_finance/presentation/widgets/driver_financial_movement_form_dialog.dart';
 import '../../domain/entities/driver.dart';
 import '../../domain/entities/driver_status_filter.dart';
 import '../cubit/drivers_cubit.dart';
@@ -66,16 +68,60 @@ class _DriversPageState extends State<DriversPage> {
   Future<void> _openDetails(Driver driver) async {
     final cubit = context.read<DriversCubit>();
     cubit.loadDriverActivity(driver);
+    cubit.loadDriverFinancialMovements(driver);
     await showDialog<void>(
       context: context,
       builder: (_) => BlocBuilder<DriversCubit, DriversState>(
         builder: (context, state) => DriverDetailsDialog(
           driver: driver,
           state: state is DriversLoaded ? state : null,
+          onAddAdvance: () => _openFinancialMovementForm(
+            driver: driver,
+            movementType: DriverFinancialMovementType.advance,
+          ),
+          onAddDeduction: () => _openFinancialMovementForm(
+            driver: driver,
+            movementType: DriverFinancialMovementType.deduction,
+          ),
         ),
       ),
     );
     cubit.clearDriverActivity();
+  }
+
+  Future<void> _openFinancialMovementForm({
+    required Driver driver,
+    required DriverFinancialMovementType movementType,
+  }) async {
+    await showDialog<void>(
+      context: context,
+      builder: (_) => DriverFinancialMovementFormDialog(
+        movementType: movementType,
+        onSubmit: ({
+          required amount,
+          required movementDate,
+          tripId,
+          notes,
+        }) {
+          final cubit = context.read<DriversCubit>();
+          if (movementType.isAdvance) {
+            return cubit.addDriverAdvance(
+              driver: driver,
+              amount: amount,
+              movementDate: movementDate,
+              notes: notes,
+            );
+          }
+          return cubit.addDriverDeduction(
+            driver: driver,
+            amount: amount,
+            movementDate: movementDate,
+            tripId: tripId,
+            notes: notes,
+          );
+        },
+      ),
+    );
   }
 
   @override
