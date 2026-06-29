af133aee3db415e7c53574ee453f16c292c438d3
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-29 17:00:59 +0400

    Issue #50 - Add category model skeleton

---
+++ lib/features/company_expenses/data/models/company_expense_category_model.dart
@@ -0,0 +1,13 @@
+class CompanyExpenseCategoryModel {
+  final String id;
+  final String companyId;
+  final String name;
+  final bool isActive;
+
+  const CompanyExpenseCategoryModel({
+    required this.id,
+    required this.companyId,
+    required this.name,
+    required this.isActive,
+  });
+}
