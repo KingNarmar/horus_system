2a6e570531863faa4a54deef61bb7015eb31738d
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-29 17:30:47 +0400

    Company expenses UI - Wire cubit dependency

--- lib/features/company_expenses/di/company_expenses_dependencies.dart
+++ lib/features/company_expenses/di/company_expenses_dependencies.dart
@@ -4,6 +4,7 @@ import '../data/datasources/company_expenses_remote_data_source.dart';
 import '../data/repositories/company_expenses_repository_impl.dart';
 import '../domain/repositories/company_expenses_repository.dart';
 import '../domain/usecases/company_expenses_usecases.dart';
+import '../presentation/cubit/company_expenses_cubit.dart';
 
 abstract final class CompanyExpensesDependencies {
   static CompanyExpensesRemoteDataSource createRemoteDataSource() {
@@ -18,6 +19,22 @@ abstract final class CompanyExpensesDependencies {
     return CompanyExpensesRepositoryImpl(
       remoteDataSource: createRemoteDataSource(),
       createAuditLogUseCase: createAuditLogUseCase,
+    );
+  }
+
+  static CompanyExpensesCubit createCubit({
+    required CreateAuditLogUseCase createAuditLogUseCase,
+  }) {
+    final repository = createRepository(
+      createAuditLogUseCase: createAuditLogUseCase,
+    );
+
+    return CompanyExpensesCubit(
+      getCategoriesUseCase: createGetCategoriesUseCase(repository),
+      getExpensesUseCase: createGetExpensesUseCase(repository),
+      addExpenseUseCase: createAddExpenseUseCase(repository),
+      updateExpenseUseCase: createUpdateExpenseUseCase(repository),
+      voidExpenseUseCase: createVoidExpenseUseCase(repository),
     );
   }
 
