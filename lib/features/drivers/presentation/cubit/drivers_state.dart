5a4854e88ba6953ac25bd7eecdf83d5a10543c3b
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-29 10:20:49 +0400

    Issue 24 extend drivers state for finance

--- lib/features/drivers/presentation/cubit/drivers_state.dart
+++ lib/features/drivers/presentation/cubit/drivers_state.dart
@@ -1,6 +1,8 @@
 import '../../../../core/errors/failure.dart';
 import '../../../audit/domain/entities/audit_log.dart';
 import '../../../company/domain/entities/current_company_context.dart';
+import '../../../driver_finance/domain/entities/driver_balance.dart';
+import '../../../driver_finance/domain/entities/driver_financial_movement.dart';
 import '../../domain/entities/driver.dart';
 import '../../domain/entities/driver_status_filter.dart';
 
@@ -22,6 +24,7 @@ class DriversLoaded extends DriversState {
   final CurrentCompanyContext currentCompanyContext;
   final List<Driver> allDrivers;
   final bool canManageDrivers;
+  final bool canManageDriverFinance;
   final String searchQuery;
   final DriverStatusFilter statusFilter;
   final String? pendingActionDriverId;
@@ -29,11 +32,17 @@ class DriversLoaded extends DriversState {
   final List<AuditLog> selectedDriverActivity;
   final bool isActivityLoading;
   final Failure? activityFailure;
+  final List<DriverFinancialMovement> selectedDriverFinancialMovements;
+  final DriverBalance? selectedDriverBalance;
+  final bool isFinancialMovementsLoading;
+  final bool isSavingFinancialMovement;
+  final Failure? financialMovementsFailure;
 
   const DriversLoaded({
     required this.currentCompanyContext,
     required this.allDrivers,
     required this.canManageDrivers,
+    required this.canManageDriverFinance,
     this.searchQuery = '',
     this.statusFilter = DriverStatusFilter.active,
     this.pendingActionDriverId,
@@ -41,6 +50,11 @@ class DriversLoaded extends DriversState {
     this.selectedDriverActivity = const [],
     this.isActivityLoading = false,
     this.activityFailure,
+    this.selectedDriverFinancialMovements = const [],
+    this.selectedDriverBalance,
+    this.isFinancialMovementsLoading = false,
+    this.isSavingFinancialMovement = false,
+    this.financialMovementsFailure,
   });
 
   List<Driver> get drivers {
@@ -65,6 +79,7 @@ class DriversLoaded extends DriversState {
   DriversLoaded copyWith({
     List<Driver>? allDrivers,
     bool? canManageDrivers,
+    bool? canManageDriverFinance,
     String? searchQuery,
     DriverStatusFilter? statusFilter,
     Object? pendingActionDriverId = _notSet,
@@ -72,11 +87,18 @@ class DriversLoaded extends DriversState {
     List<AuditLog>? selectedDriverActivity,
     bool? isActivityLoading,
     Object? activityFailure = _notSet,
+    List<DriverFinancialMovement>? selectedDriverFinancialMovements,
+    Object? selectedDriverBalance = _notSet,
+    bool? isFinancialMovementsLoading,
+    bool? isSavingFinancialMovement,
+    Object? financialMovementsFailure = _notSet,
   }) {
     return DriversLoaded(
       currentCompanyContext: currentCompanyContext,
       allDrivers: allDrivers ?? this.allDrivers,
       canManageDrivers: canManageDrivers ?? this.canManageDrivers,
+      canManageDriverFinance:
+          canManageDriverFinance ?? this.canManageDriverFinance,
       searchQuery: searchQuery ?? this.searchQuery,
       statusFilter: statusFilter ?? this.statusFilter,
       pendingActionDriverId: pendingActionDriverId == _notSet
@@ -91,6 +113,18 @@ class DriversLoaded extends DriversState {
       activityFailure: activityFailure == _notSet
           ? this.activityFailure
           : activityFailure as Failure?,
+      selectedDriverFinancialMovements: selectedDriverFinancialMovements ??
+          this.selectedDriverFinancialMovements,
+      selectedDriverBalance: selectedDriverBalance == _notSet
+          ? this.selectedDriverBalance
+          : selectedDriverBalance as DriverBalance?,
+      isFinancialMovementsLoading:
+          isFinancialMovementsLoading ?? this.isFinancialMovementsLoading,
+      isSavingFinancialMovement:
+          isSavingFinancialMovement ?? this.isSavingFinancialMovement,
+      financialMovementsFailure: financialMovementsFailure == _notSet
+          ? this.financialMovementsFailure
+          : financialMovementsFailure as Failure?,
     );
   }
 }
