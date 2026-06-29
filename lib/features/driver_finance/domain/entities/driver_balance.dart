7efc155c135654410fae2e02e76a8265a3903279
Mina A Bushra <mina.bushra1990@gmail.com>
2026-06-29 10:15:24 +0400

    Issue 24 complete driver balance entity

--- lib/features/driver_finance/domain/entities/driver_balance.dart
+++ lib/features/driver_finance/domain/entities/driver_balance.dart
@@ -2,10 +2,14 @@ class DriverBalance {
   final String companyId;
   final String driverId;
   final double totalAdvances;
+  final double totalDeductions;
 
   const DriverBalance({
     required this.companyId,
     required this.driverId,
     required this.totalAdvances,
+    required this.totalDeductions,
   });
+
+  double get netBalance => totalAdvances - totalDeductions;
 }
