8394920fb1d6b2d85b341e1172ce020446a04741
Mina A Bushra <mina.bushra1990@gmail.com>
2026-07-03 15:09:08 +0400

    refactor: use centralized audit dependencies in routes di

--- lib/core/di/routes_dependencies.dart
+++ lib/core/di/routes_dependencies.dart
@@ -1,7 +1,4 @@
-import '../../features/audit/data/datasources/audit_logs_remote_data_source.dart';
-import '../../features/audit/data/repositories/audit_log_repository_impl.dart';
-import '../../features/audit/domain/usecases/create_audit_log_usecase.dart';
-import '../../features/audit/domain/usecases/get_entity_audit_logs_usecase.dart';
+import '../../features/audit/di/audit_dependencies.dart';
 import '../../features/routes/data/datasources/routes_remote_data_source.dart';
 import '../../features/routes/data/repositories/routes_repository_impl.dart';
 import '../../features/routes/domain/usecases/routes_usecases.dart';
@@ -12,15 +9,10 @@ abstract final class RoutesDependencies {
   static RoutesCubit createRoutesCubit() {
     final client = SupabaseClientProvider.client;
 
-    final auditRemoteDataSource = SupabaseAuditLogsRemoteDataSource(client);
-    final auditRepository = AuditLogRepositoryImpl(
-      remoteDataSource: auditRemoteDataSource,
-    );
-
     final routesRemoteDataSource = SupabaseRoutesRemoteDataSource(client);
     final routesRepository = RoutesRepositoryImpl(
       remoteDataSource: routesRemoteDataSource,
-      createAuditLogUseCase: CreateAuditLogUseCase(auditRepository),
+      createAuditLogUseCase: AuditDependencies.createAuditLogUseCase,
     );
 
     return RoutesCubit(
@@ -28,7 +20,7 @@ abstract final class RoutesDependencies {
       saveRouteUseCase: SaveRouteUseCase(routesRepository),
       deactivateRouteUseCase: DeactivateRouteUseCase(routesRepository),
       reactivateRouteUseCase: ReactivateRouteUseCase(routesRepository),
-      getRouteAuditLogsUseCase: GetEntityAuditLogsUseCase(auditRepository),
+      getRouteAuditLogsUseCase: AuditDependencies.getEntityAuditLogsUseCase,
     );
   }
 }
