60545da62c177da0cb9a62882e3e9a0eb5e1b3be
Mina A Bushra <mina.bushra1990@gmail.com>
2026-07-03 14:29:47 +0400

    refactor: add company expense presentation constants

---
+++ lib/features/company_expenses/presentation/constants/company_expense_presentation_constants.dart
@@ -0,0 +1,5 @@
+abstract final class CompanyExpensePresentationConstants {
+  static const int expenseDatePickerFirstYear = 2020;
+  static const int expenseDatePickerLastYear = 2100;
+  static const int notesMaxLines = 3;
+}
