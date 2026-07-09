80acbe8bf833786e851a8fced0158ee993dc0735
Mina A Bushra <mina.bushra1990@gmail.com>
2026-07-09 16:54:43 +0400

    feat(driver-settlements): add domain foundation

---
+++ lib/features/driver_settlements/domain/entities/driver_settlement_source_snapshot.dart
@@ -0,0 +1,19 @@
+import 'driver_settlement_item.dart';
+
+class DriverSettlementSourceSnapshot {
+  final double openingDriverBalance;
+  final double advancesTotal;
+  final double driverPaidTripExpensesTotal;
+  final double returnedCashTotal;
+  final double deductionsTotal;
+  final List<DriverSettlementItem> sourceItems;
+
+  const DriverSettlementSourceSnapshot({
+    this.openingDriverBalance = 0,
+    this.advancesTotal = 0,
+    this.driverPaidTripExpensesTotal = 0,
+    this.returnedCashTotal = 0,
+    this.deductionsTotal = 0,
+    this.sourceItems = const [],
+  });
+}
