f0e676acbf161ddb6077e048556ad5bf247182cd
Mina A Bushra <mina.bushra1990@gmail.com>
2026-07-12 20:45:43 +0400

    feat: add settlement presentation constants

---
+++ lib/features/driver_settlements/presentation/constants/driver_settlement_presentation_constants.dart
@@ -0,0 +1,6 @@
+abstract final class DriverSettlementPresentationConstants {
+  static const int datePickerFirstYear = 2020;
+  static const int datePickerFutureYears = 5;
+  static const int notesMaxLines = 3;
+  static const int voidReasonMaxLines = 3;
+}
