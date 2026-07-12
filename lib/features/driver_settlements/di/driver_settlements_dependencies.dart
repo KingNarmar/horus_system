62907d4e30f2eb3ad020a33339fb7900171ff249
Mina A Bushra <mina.bushra1990@gmail.com>
2026-07-12 20:42:55 +0400

    feat: wire driver settlements cubit dependencies

--- lib/features/driver_settlements/di/driver_settlements_dependencies.dart
+++ lib/features/driver_settlements/di/driver_settlements_dependencies.dart
@@ -4,6 +4,7 @@ import '../data/datasources/driver_settlements_remote_data_source.dart';
 import '../data/repositories/driver_settlements_repository_impl.dart';
 import '../domain/repositories/driver_settlements_repository.dart';
 import '../domain/usecases/driver_settlement_usecases.dart';
+import '../presentation/cubit/driver_settlements_cubit.dart';
 
 abstract final class DriverSettlementsDependencies {
   static DriverSettlementsRepository createRepository() {
@@ -16,8 +17,32 @@ abstract final class DriverSettlementsDependencies {
     );
   }
 
+  static DriverSettlementsCubit createCubit() {
+    final repository = createRepository();
+    return DriverSettlementsCubit(
+      getDriverSettlementsUseCase: GetDriverSettlementsUseCase(repository),
+      getDriverOptionsUseCase: GetDriverSettlementDriverOptionsUseCase(
+        repository,
+      ),
+      getDriverSettlementDetailsUseCase:
+          GetDriverSettlementDetailsUseCase(repository),
+      calculatePreviewUseCase: CalculateDriverSettlementPreviewUseCase(
+        repository,
+      ),
+      createDraftUseCase: CreateDriverSettlementDraftUseCase(repository),
+      finalizeSettlementUseCase: FinalizeDriverSettlementUseCase(repository),
+      voidSettlementUseCase: VoidDriverSettlementUseCase(repository),
+      getEntityAuditLogsUseCase: AuditDependencies.getEntityAuditLogsUseCase,
+    );
+  }
+
   static GetDriverSettlementsUseCase createGetDriverSettlementsUseCase() {
     return GetDriverSettlementsUseCase(createRepository());
+  }
+
+  static GetDriverSettlementDriverOptionsUseCase
+  createGetDriverSettlementDriverOptionsUseCase() {
+    return GetDriverSettlementDriverOptionsUseCase(createRepository());
   }
 
   static GetDriverSettlementDetailsUseCase
