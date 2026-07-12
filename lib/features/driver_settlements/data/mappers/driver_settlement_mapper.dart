69d261a6fc341b2aa8aa3d22266b85fc1cbcd792
Mina A Bushra <mina.bushra1990@gmail.com>
2026-07-12 17:54:57 +0400

    fix(driver-settlements): preserve local dates in mapper

--- lib/features/driver_settlements/data/mappers/driver_settlement_mapper.dart
+++ lib/features/driver_settlements/data/mappers/driver_settlement_mapper.dart
@@ -178,8 +178,7 @@ extension DriverSettlementVoidDataMapper on DriverSettlementVoidData {
 }
 
 String _dateOnly(DateTime value) {
-  final utc = value.toUtc();
-  return '${utc.year.toString().padLeft(4, '0')}-'
-      '${utc.month.toString().padLeft(2, '0')}-'
-      '${utc.day.toString().padLeft(2, '0')}';
+  return '${value.year.toString().padLeft(4, '0')}-'
+      '${value.month.toString().padLeft(2, '0')}-'
+      '${value.day.toString().padLeft(2, '0')}';
 }
