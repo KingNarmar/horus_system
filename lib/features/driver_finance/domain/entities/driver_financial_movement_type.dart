fd7ef17cd2a1f22bbf5fe6c182b46da0d7ffb142
Mina A Bushra <mina.bushra1990@gmail.com>
2026-07-13 16:54:22 +0400

    refactor: define explicit driver finance movement types

--- lib/features/driver_finance/domain/entities/driver_financial_movement_type.dart
+++ lib/features/driver_finance/domain/entities/driver_financial_movement_type.dart
@@ -1,25 +1,33 @@
 enum DriverFinancialMovementType {
   advance,
-  deduction,
+  driverCharge,
+  cashReturn,
 }
 
 extension DriverFinancialMovementTypeX on DriverFinancialMovementType {
   String get value {
     return switch (this) {
       DriverFinancialMovementType.advance => 'advance',
-      DriverFinancialMovementType.deduction => 'deduction',
+      DriverFinancialMovementType.driverCharge => 'driver_charge',
+      DriverFinancialMovementType.cashReturn => 'cash_return',
     };
   }
 
   bool get isAdvance => this == DriverFinancialMovementType.advance;
 
-  bool get isDeduction => this == DriverFinancialMovementType.deduction;
+  bool get isDriverCharge => this == DriverFinancialMovementType.driverCharge;
+
+  bool get isCashReturn => this == DriverFinancialMovementType.cashReturn;
+
+  bool get canLinkTrip => isDriverCharge;
 }
 
 DriverFinancialMovementType driverFinancialMovementTypeFromValue(String value) {
   return switch (value) {
     'advance' => DriverFinancialMovementType.advance,
-    'deduction' => DriverFinancialMovementType.deduction,
+    'driver_charge' || 'deduction' =>
+      DriverFinancialMovementType.driverCharge,
+    'cash_return' => DriverFinancialMovementType.cashReturn,
     _ => throw ArgumentError.value(
       value,
       'value',
