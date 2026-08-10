16cca7ac287457cbcb382ccfdc5108161f16b0eb
Mina A Bushra <mina.bushra1990@gmail.com>
2026-08-10 23:29:56 +0400

    feat(payments): add payment use case params

---
+++ lib/features/payments/domain/usecases/payment_params.dart
@@ -0,0 +1,33 @@
+import '../../../company/domain/entities/current_company_context.dart';
+
+final class GetPaymentsParams {
+  final CurrentCompanyContext currentCompanyContext;
+
+  const GetPaymentsParams({required this.currentCompanyContext});
+}
+
+final class GetPayableInvoicesParams {
+  final CurrentCompanyContext currentCompanyContext;
+
+  const GetPayableInvoicesParams({required this.currentCompanyContext});
+}
+
+final class RegisterPaymentParams {
+  final CurrentCompanyContext currentCompanyContext;
+  final String invoiceId;
+  final String paymentMethodId;
+  final DateTime paymentDate;
+  final String amountText;
+  final String? referenceNumber;
+  final String? notes;
+
+  const RegisterPaymentParams({
+    required this.currentCompanyContext,
+    required this.invoiceId,
+    required this.paymentMethodId,
+    required this.paymentDate,
+    required this.amountText,
+    this.referenceNumber,
+    this.notes,
+  });
+}
