f824440e0fe623e3a0b62c110c4681e5c3245867
Mina A Bushra <mina.bushra1990@gmail.com>
2026-08-22 23:24:06 +0400

    refactor: extract trip expense failure mapper

---
+++ lib/features/expenses/data/repositories/trip_expense_repository_failure_mapper.dart
@@ -0,0 +1,20 @@
+import 'package:supabase_flutter/supabase_flutter.dart' show PostgrestException;
+
+import '../../../../core/errors/common_failures.dart';
+import '../../../../core/errors/failure.dart';
+import '../../../../core/errors/failure_codes.dart';
+
+final class TripExpenseRepositoryFailureMapper {
+  const TripExpenseRepositoryFailureMapper();
+
+  Failure fromPostgrest(PostgrestException error) {
+    return ServerFailure(
+      code: error.code ?? FailureCodes.serverError,
+      message: error.message,
+    );
+  }
+
+  Failure fromUnexpected(Object error) {
+    return UnexpectedFailure(message: error.toString());
+  }
+}
