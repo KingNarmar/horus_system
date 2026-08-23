80e238bc398211ba5569b188da717d32720455b8
Mina A Bushra <mina.bushra1990@gmail.com>
2026-08-23 17:08:22 +0400

    Refactor Driver Finance repository responsibilities

--- lib/features/driver_finance/data/repositories/driver_finance_repository_impl.dart
+++ lib/features/driver_finance/data/repositories/driver_finance_repository_impl.dart
@@ -1,35 +1,30 @@
 import 'package:supabase_flutter/supabase_flutter.dart' show PostgrestException;
 
-import '../../../../core/errors/common_failures.dart';
-import '../../../../core/errors/failure.dart';
-import '../../../../core/errors/failure_codes.dart';
 import '../../../../core/utils/result.dart';
-import '../../../audit/domain/entities/audit_action.dart';
-import '../../../audit/domain/entities/audit_entity_type.dart';
-import '../../../audit/domain/entities/audit_log_write_data.dart';
-import '../../../audit/domain/entities/audit_module.dart';
 import '../../../audit/domain/usecases/create_audit_log_usecase.dart';
 import '../../domain/entities/driver_finance_trip_option.dart';
 import '../../domain/entities/driver_financial_movement.dart';
-import '../../domain/entities/driver_financial_movement_type.dart';
 import '../../domain/entities/driver_financial_movement_write_data.dart';
 import '../../domain/repositories/driver_finance_repository.dart';
 import '../datasources/driver_finance_remote_data_source.dart';
 import '../mappers/driver_finance_trip_option_mapper.dart';
 import '../mappers/driver_financial_movement_mapper.dart';
-import '../models/driver_financial_movement_model.dart';
-
-const _driverFinancialMovementEntityKey = 'driver_financial_movement';
-const _driverFinanceMovementAddedEvent = 'driver_finance_movement_added';
+import 'driver_finance_repository_audit_writer.dart';
+import 'driver_finance_repository_failure_mapper.dart';
 
 class DriverFinanceRepositoryImpl implements DriverFinanceRepository {
   final DriverFinanceRemoteDataSource remoteDataSource;
   final CreateAuditLogUseCase createAuditLogUseCase;
+  final DriverFinanceRepositoryFailureMapper _failureMapper;
 
   const DriverFinanceRepositoryImpl({
     required this.remoteDataSource,
     required this.createAuditLogUseCase,
-  });
+  }) : _failureMapper = const DriverFinanceRepositoryFailureMapper();
+
+  DriverFinanceRepositoryAuditWriter get _auditWriter {
+    return DriverFinanceRepositoryAuditWriter(createAuditLogUseCase);
+  }
 
   @override
   Future<Result<List<DriverFinancialMovement>>> getDriverMovements({
@@ -66,11 +61,9 @@ class DriverFinanceRepositoryImpl implements DriverFinanceRepository {
   }) {
     return _guard(() async {
       final model = await remoteDataSource.addDriverMovement(data: data);
-      final auditFailure = await _writeAudit(
-        companyId: model.companyId,
-        driverId: model.driverId,
-        actorRole: actorRole,
+      final auditFailure = await _auditWriter.writeAdded(
         movement: model,
+        actorRole: actorRole,
       );
 
       if (auditFailure != null) {
@@ -80,50 +73,13 @@ class DriverFinanceRepositoryImpl implements DriverFinanceRepository {
     });
   }
 
-  Future<Failure?> _writeAudit({
-    required String companyId,
-    required String driverId,
-    required String actorRole,
-    required DriverFinancialMovementModel movement,
-  }) async {
-    final result = await createAuditLogUseCase(
-      CreateAuditLogParams(
-        data: AuditLogWriteData(
-          companyId: companyId,
-          actorRole: actorRole,
-          module: AuditModule.drivers,
-          entityType: AuditEntityType.driver,
-          entityId: driverId,
-          entityDisplayName: _driverFinancialMovementEntityKey,
-          action: AuditAction.created,
-          description: _driverFinanceMovementAddedEvent,
-          newValues: movement.toAuditValues(),
-          metadata: {
-            'audit_event': _driverFinanceMovementAddedEvent,
-            'movement_id': movement.id,
-            'movement_type': movement.type.value,
-            'amount': movement.amount,
-            'trip_id': movement.tripId,
-          },
-        ),
-      ),
-    );
-
-    return result.failureOrNull;
-  }
-
   Future<Result<T>> _guard<T>(Future<Result<T>> Function() action) async {
     try {
       return await action();
     } on PostgrestException catch (error) {
-      return FailureResult(
-        ServerFailure(
-          code: error.code ?? FailureCodes.serverError,
-          message: error.message,
-        ),
-      );
+      return FailureResult(_failureMapper.fromMovementPostgrest(error));
     } catch (error) {
-      return FailureResult(UnexpectedFailure(message: error.toString()));
+      return FailureResult(_failureMapper.fromUnexpected(error));
     }
   }
 }
