d4b3c42a2393ed89e3053786fbd0bcc1c9e25daf
Mina A Bushra <mina.bushra1990@gmail.com>
2026-08-25 20:21:20 +0400

    refactor: simplify company users repository boundary

--- lib/features/company/data/repositories/company_users_repository_impl.dart
+++ lib/features/company/data/repositories/company_users_repository_impl.dart
@@ -1,12 +1,11 @@
-import 'package:horus_system/core/errors/failure_codes.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' show PostgrestException;
 
-import '../../../../core/errors/common_failures.dart';
 import '../../../../core/utils/result.dart';
 import '../../domain/entities/company_user.dart';
 import '../../domain/repositories/company_users_repository.dart';
 import '../datasources/company_users_remote_data_source.dart';
 import '../mappers/company_user_mapper.dart';
+import 'company_users_repository_failure_mapper.dart';
 
 class CompanyUsersRepositoryImpl implements CompanyUsersRepository {
   final CompanyUsersRemoteDataSource _remoteDataSource;
@@ -15,36 +14,26 @@ class CompanyUsersRepositoryImpl implements CompanyUsersRepository {
     required CompanyUsersRemoteDataSource remoteDataSource,
   }) : _remoteDataSource = remoteDataSource;
 
+  static const _failureMapper = CompanyUsersRepositoryFailureMapper();
+
   @override
   Future<Result<List<CompanyUser>>> getCompanyUsers({
     required String companyId,
-  }) async {
+  }) {
+    return _guard(
+      () async => (await _remoteDataSource.getCompanyUsers(
+        companyId: companyId,
+      )).map((model) => model.toEntity()).toList(),
+    );
+  }
+
+  Future<Result<T>> _guard<T>(Future<T> Function() action) async {
     try {
-      final normalizedCompanyId = companyId.trim();
-
-      if (normalizedCompanyId.isEmpty) {
-        return const FailureResult(
-          ValidationFailure(
-            code: FailureCodes.validationCompanyIdRequired,
-            message: 'Company id is required.',
-          ),
-        );
-      }
-
-      final models = await _remoteDataSource.getCompanyUsers(
-        companyId: normalizedCompanyId,
-      );
-
-      return Success(models.map((model) => model.toEntity()).toList());
+      return Success(await action());
     } on PostgrestException catch (error) {
-      return FailureResult(
-        ServerFailure(
-          code: error.code ?? FailureCodes.serverError,
-          message: error.message,
-        ),
-      );
+      return FailureResult(_failureMapper.fromPostgrest(error));
     } catch (error) {
-      return FailureResult(UnexpectedFailure(message: error.toString()));
+      return FailureResult(_failureMapper.fromUnexpected(error));
     }
   }
 }
