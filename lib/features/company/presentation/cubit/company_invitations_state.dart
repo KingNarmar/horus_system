a25a041da7dcc2b6c89067fc98659393374c8dce
Mina A Bushra <mina.bushra1990@gmail.com>
2026-08-30 16:54:44 +0400

    feat: add company invitations state

---
+++ lib/features/company/presentation/cubit/company_invitations_state.dart
@@ -0,0 +1,43 @@
+import '../../../../core/errors/failure.dart';
+import '../../domain/entities/company_invitation.dart';
+
+sealed class CompanyInvitationsState {
+  final String? companyId;
+
+  const CompanyInvitationsState({this.companyId});
+}
+
+class CompanyInvitationsInitial extends CompanyInvitationsState {
+  const CompanyInvitationsInitial();
+}
+
+class CompanyInvitationsLoading extends CompanyInvitationsState {
+  const CompanyInvitationsLoading({required super.companyId});
+}
+
+class CompanyInvitationsLoaded extends CompanyInvitationsState {
+  final List<CompanyInvitation> invitations;
+
+  const CompanyInvitationsLoaded({
+    required super.companyId,
+    required this.invitations,
+  });
+}
+
+class CompanyInvitationsCommandInProgress extends CompanyInvitationsState {
+  final List<CompanyInvitation> invitations;
+
+  const CompanyInvitationsCommandInProgress({
+    required super.companyId,
+    required this.invitations,
+  });
+}
+
+class CompanyInvitationsFailure extends CompanyInvitationsState {
+  final Failure failure;
+
+  const CompanyInvitationsFailure({
+    required super.companyId,
+    required this.failure,
+  });
+}
