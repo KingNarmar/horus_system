6a3e9af54599bfc396f8ee987a1d81ea0fb50a16
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-01 20:21:41 +0400

    refactor: route company creation through rpc constants

--- lib/features/company/data/constants/company_rpc_constants.dart
+++ lib/features/company/data/constants/company_rpc_constants.dart
@@ -1,8 +1,15 @@
 abstract final class CompanyRpcConstants {
+  static const createCompany = 'create_company_with_initial_owner';
   static const updateRegionalSettings = 'update_company_regional_settings';
   static const getBusinessDate = 'get_company_business_date';
 
   static const companyId = 'p_company_id';
+  static const companyName = 'p_name';
+  static const businessType = 'p_business_type';
+  static const phone = 'p_phone';
+  static const email = 'p_email';
+  static const country = 'p_country';
+  static const city = 'p_city';
   static const baseCurrencyCode = 'p_base_currency_code';
   static const baseCurrencyFractionDigits = 'p_base_currency_fraction_digits';
   static const businessTimezone = 'p_business_timezone';
