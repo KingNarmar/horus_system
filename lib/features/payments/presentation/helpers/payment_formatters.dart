c774fab749dcf3f13739d01f669ce4de90abd2d4
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-08 15:59:17 +0400

    PC-01: format payment timestamps from company-local values

--- lib/features/payments/presentation/helpers/payment_formatters.dart
+++ lib/features/payments/presentation/helpers/payment_formatters.dart
@@ -1,7 +1,9 @@
 import 'package:intl/intl.dart';
 
 import '../../../../core/domain/value_objects/business_date.dart';
+import '../../../../core/domain/value_objects/business_local_date_time.dart';
 import '../../../../core/utils/business_date_date_time_adapter.dart';
+import '../../../../core/utils/business_local_date_time_date_time_adapter.dart';
 
 String formatPaymentDate(BusinessDate date, String localeName) {
   return DateFormat.yMMMd(
@@ -9,6 +11,11 @@ String formatPaymentDate(BusinessDate date, String localeName) {
   ).format(BusinessDateDateTimeAdapter.toDateTime(date));
 }
 
-String formatPaymentDateTime(DateTime date, String localeName) {
-  return DateFormat.yMMMd(localeName).add_jm().format(date.toLocal());
+String formatPaymentDateTime(
+  BusinessLocalDateTime date,
+  String localeName,
+) {
+  return DateFormat.yMMMd(localeName).add_jm().format(
+    BusinessLocalDateTimeDateTimeAdapter.toDateTime(date),
+  );
 }
