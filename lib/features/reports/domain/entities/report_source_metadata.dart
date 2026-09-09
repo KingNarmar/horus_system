29da2729bb9486540bf3c38cd434b65ba261f414
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-09 21:41:45 +0400

    refactor: split operational and financial report metadata

--- lib/features/reports/domain/entities/report_source_metadata.dart
+++ lib/features/reports/domain/entities/report_source_metadata.dart
@@ -2,8 +2,8 @@ import '../../../../core/domain/value_objects/currency_code.dart';
 
 final class ReportSourceMetadata {
   final String companyId;
-  final CurrencyCode? currency;
-  final int? baseCurrencyFractionDigits;
+  final CurrencyCode currency;
+  final int baseCurrencyFractionDigits;
   final String businessTimezone;
   final DateTime businessDate;
   final DateTime? fromDate;
@@ -13,6 +13,22 @@ final class ReportSourceMetadata {
     required this.companyId,
     required this.currency,
     required this.baseCurrencyFractionDigits,
+    required this.businessTimezone,
+    required this.businessDate,
+    required this.fromDate,
+    required this.toDate,
+  });
+}
+
+final class OperationalReportSourceMetadata {
+  final String companyId;
+  final String businessTimezone;
+  final DateTime businessDate;
+  final DateTime? fromDate;
+  final DateTime? toDate;
+
+  const OperationalReportSourceMetadata({
+    required this.companyId,
     required this.businessTimezone,
     required this.businessDate,
     required this.fromDate,
