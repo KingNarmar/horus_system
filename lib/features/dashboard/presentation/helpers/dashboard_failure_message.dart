14c9a3c5450850c355f38c33dc60ec13aa3974c5
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-13 15:26:06 +0400

    refactor: use shared financial readiness copy in dashboard

--- lib/features/dashboard/presentation/helpers/dashboard_failure_message.dart
+++ lib/features/dashboard/presentation/helpers/dashboard_failure_message.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import '../../../../core/errors/common_failures.dart';
 import '../../../../core/errors/failure.dart';
 import '../../../../core/localization/app_localizations_extension.dart';
+import '../../../../core/localization/financial_readiness_localizations.dart';
 import '../../../company/domain/failures/company_failure_codes.dart';
 import '../../domain/failures/dashboard_failure_codes.dart';
 import '../localization/dashboard_localizations.dart';
@@ -18,7 +19,7 @@ String dashboardFailureMessage(BuildContext context, Failure failure) {
     DashboardFailureCodes.conflictFinancialDataInvalid =>
       strings.financialDataInvalidFailure,
     CompanyFailureCodes.conflictRegionalSettingsNotConfigured =>
-      strings.regionalSettingsFailure,
+      context.financialReadinessL10n.configurationRequired,
     CompanyFailureCodes.notFound => strings.companyNotFoundFailure,
     _ => _safeFallback(context, failure, strings),
   };
