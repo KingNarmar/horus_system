b2b161d6eaea6f533a28dcd0ea3d58da48ca0e28
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-16 17:10:45 +0400

    refactor: type compensation end date

--- lib/features/drivers/domain/repositories/driver_compensation_repository.dart
+++ lib/features/drivers/domain/repositories/driver_compensation_repository.dart
@@ -1,5 +1,6 @@
 import '../../../../core/documents/domain/entities/business_document_access.dart';
 import '../../../../core/documents/domain/entities/business_document_file.dart';
+import '../../../../core/domain/value_objects/business_date.dart';
 import '../../../../core/utils/result.dart';
 import '../entities/driver_compensation_revision.dart';
 import '../entities/driver_compensation_write_data.dart';
@@ -21,7 +22,7 @@ abstract class DriverCompensationRepository {
     required String revisionId,
     required String driverId,
     required String actorRole,
-    required String effectiveTo,
+    required BusinessDate effectiveTo,
   });
 
   Future<Result<DriverCompensationRevision>> attachContractDocument({
