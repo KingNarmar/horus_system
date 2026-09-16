cdc1de80d41592d7feba966bc3629e73b443bd9a
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-16 15:54:50 +0400

    style: remove unused Trip localization import

--- lib/features/trips/presentation/helpers/trips_failure_message.dart
+++ lib/features/trips/presentation/helpers/trips_failure_message.dart
@@ -6,7 +6,6 @@ import '../../../../core/localization/app_localizations_extension.dart';
 import '../../../../core/localization/financial_readiness_localizations.dart';
 import '../../../company/domain/failures/company_failure_codes.dart';
 import '../../domain/failures/trip_failure_codes.dart';
-import '../localization/trips_localizations_x.dart';
 
 String tripsFailureMessage(BuildContext context, Failure failure) {
   final l10n = context.l10n;
