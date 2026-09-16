303bbb47e7862aa0fa87c56efc70038a1567042a
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-16 17:10:09 +0400

    feat: add driver compensation repository contract

---
+++ lib/features/drivers/domain/repositories/driver_compensation_repository.dart
@@ -0,0 +1,37 @@
+import '../../../../core/documents/domain/entities/business_document_access.dart';
+import '../../../../core/documents/domain/entities/business_document_file.dart';
+import '../../../../core/utils/result.dart';
+import '../entities/driver_compensation_revision.dart';
+import '../entities/driver_compensation_write_data.dart';
+
+abstract class DriverCompensationRepository {
+  Future<Result<List<DriverCompensationRevision>>> getHistory({
+    required String companyId,
+    required String driverId,
+  });
+
+  Future<Result<DriverCompensationRevision>> createRevision({
+    required DriverCompensationWriteData data,
+    required String actorRole,
+    BusinessDocumentFile? contractDocument,
+  });
+
+  Future<Result<DriverCompensationRevision>> endRevision({
+    required String companyId,
+    required String revisionId,
+    required String driverId,
+    required String actorRole,
+    required String effectiveTo,
+  });
+
+  Future<Result<DriverCompensationRevision>> attachContractDocument({
+    required DriverCompensationRevision revision,
+    required String actorRole,
+    required BusinessDocumentFile document,
+  });
+
+  Future<Result<BusinessDocumentAccess>> createContractDocumentAccess({
+    required String companyId,
+    required DriverCompensationRevision revision,
+  });
+}
