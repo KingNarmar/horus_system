0775814088f80c91f1951c355ba510d71f1077ab
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-17 17:07:51 +0400

    PC-09: add exact money driver balance calculation

--- lib/core/domain/services/driver_balance_calculator.dart
+++ lib/core/domain/services/driver_balance_calculator.dart
@@ -1,3 +1,5 @@
+import '../value_objects/money.dart';
+
 class DriverBalanceCalculator {
   const DriverBalanceCalculator();
 
@@ -17,6 +19,22 @@ class DriverBalanceCalculator {
           cashReturned +
           salaryRecovery,
     );
+  }
+
+  Money calculateMoney({
+    required Money openingBalance,
+    required Money advancesReceived,
+    required Money driverCharges,
+    required Money creditedTripExpenses,
+    required Money cashReturned,
+    required Money salaryRecovery,
+  }) {
+    return openingBalance
+        .subtract(advancesReceived)
+        .subtract(driverCharges)
+        .add(creditedTripExpenses)
+        .add(cashReturned)
+        .add(salaryRecovery);
   }
 
   double roundMoney(double value) {
