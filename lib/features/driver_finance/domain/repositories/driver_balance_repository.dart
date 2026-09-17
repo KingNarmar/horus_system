1bf810f6c5a3e58c24795c4003d9e642d0355dc8
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-17 18:48:34 +0400

    refactor: keep legacy driver balance repository focused

--- lib/features/driver_finance/domain/repositories/driver_balance_repository.dart
+++ lib/features/driver_finance/domain/repositories/driver_balance_repository.dart
@@ -1,22 +1,11 @@
 import '../../../../core/domain/value_objects/business_date.dart';
-import '../../../../core/domain/value_objects/currency_code.dart';
 import '../../../../core/utils/result.dart';
 import '../entities/driver_balance.dart';
-import '../entities/driver_money_balance.dart';
 
 abstract class DriverBalanceRepository {
   Future<Result<DriverBalance>> getCanonicalDriverBalance({
     required String companyId,
     required String driverId,
-    required BusinessDate beforeExclusive,
-    BusinessDate? checkpointBeforeExclusive,
-  });
-
-  Future<Result<DriverMoneyBalance>> getCanonicalDriverMoneyBalance({
-    required String companyId,
-    required String driverId,
-    required CurrencyCode currency,
-    required int currencyFractionDigits,
     required BusinessDate beforeExclusive,
     BusinessDate? checkpointBeforeExclusive,
   });
