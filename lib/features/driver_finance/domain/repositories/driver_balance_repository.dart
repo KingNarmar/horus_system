16db4c8c6199666b467d393d9866db27485babda
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-17 17:35:01 +0400

    feat: expose exact canonical driver balance repository contract

--- lib/features/driver_finance/domain/repositories/driver_balance_repository.dart
+++ lib/features/driver_finance/domain/repositories/driver_balance_repository.dart
@@ -1,11 +1,21 @@
 import '../../../../core/domain/value_objects/business_date.dart';
 import '../../../../core/utils/result.dart';
 import '../entities/driver_balance.dart';
+import '../entities/driver_money_balance.dart';
 
 abstract class DriverBalanceRepository {
   Future<Result<DriverBalance>> getCanonicalDriverBalance({
     required String companyId,
     required String driverId,
+    required BusinessDate beforeExclusive,
+    BusinessDate? checkpointBeforeExclusive,
+  });
+
+  Future<Result<DriverMoneyBalance>> getCanonicalDriverMoneyBalance({
+    required String companyId,
+    required String driverId,
+    required String currencyCode,
+    required int currencyFractionDigits,
     required BusinessDate beforeExclusive,
     BusinessDate? checkpointBeforeExclusive,
   });
