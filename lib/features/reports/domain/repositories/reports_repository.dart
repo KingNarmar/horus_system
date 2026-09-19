06d60de4542efaa8483785f20b9236680985aceb
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-19 16:30:37 +0400

    refactor(pc16): align reporting business date contracts

--- lib/features/reports/domain/repositories/reports_repository.dart
+++ lib/features/reports/domain/repositories/reports_repository.dart
@@ -1,3 +1,4 @@
+import '../../../../core/domain/value_objects/business_date.dart';
 import '../../../../core/utils/result.dart';
 import '../entities/open_invoices_report.dart';
 import '../entities/operational_trip_report.dart';
@@ -7,25 +8,25 @@ import '../entities/trip_net_profit_report.dart';
 abstract interface class ReportsRepository {
   Future<Result<OperationalTripReportSource>> getOperationalTripSource({
     required String companyId,
-    required DateTime? fromDate,
-    required DateTime? toDate,
+    required BusinessDate? fromDate,
+    required BusinessDate? toDate,
   });
 
   Future<Result<TripExpensesReportSource>> getTripExpensesSource({
     required String companyId,
-    required DateTime? fromDate,
-    required DateTime? toDate,
+    required BusinessDate? fromDate,
+    required BusinessDate? toDate,
   });
 
   Future<Result<TripNetProfitReportSource>> getTripNetProfitSource({
     required String companyId,
-    required DateTime? fromDate,
-    required DateTime? toDate,
+    required BusinessDate? fromDate,
+    required BusinessDate? toDate,
   });
 
   Future<Result<OpenInvoicesReportSource>> getOpenInvoicesSource({
     required String companyId,
-    required DateTime? fromDate,
-    required DateTime? toDate,
+    required BusinessDate? fromDate,
+    required BusinessDate? toDate,
   });
 }
