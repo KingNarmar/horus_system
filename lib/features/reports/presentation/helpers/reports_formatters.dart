ddf075b4d588bff9edcd04a527c2977d73e22d1d
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-19 16:32:09 +0400

    feat(pc16): add route reporting over business dates

--- lib/features/reports/presentation/helpers/reports_formatters.dart
+++ lib/features/reports/presentation/helpers/reports_formatters.dart
@@ -1,10 +1,14 @@
 import 'package:intl/intl.dart';
 
+import '../../../../core/domain/value_objects/business_date.dart';
 import '../../../../core/domain/value_objects/money.dart';
+import '../../../../core/utils/business_date_date_time_adapter.dart';
 import '../../../../core/localization/money_formatter.dart';
 
-String formatReportDate(DateTime date, String localeName) {
-  return DateFormat.yMMMd(localeName).format(date);
+String formatReportDate(BusinessDate date, String localeName) {
+  return DateFormat.yMMMd(
+    localeName,
+  ).format(BusinessDateDateTimeAdapter.toDateTime(date));
 }
 
 String formatReportMoney({
