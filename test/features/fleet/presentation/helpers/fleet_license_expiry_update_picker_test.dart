8127a0387c0fc7d8c512292fcea3cc4f4c367765
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-19 20:33:10 +0400

    test: cover trusted fleet license date bounds

---
+++ test/features/fleet/presentation/helpers/fleet_license_expiry_update_picker_test.dart
@@ -0,0 +1,32 @@
+import 'package:horus_system/core/constants/app_date_constraints.dart';
+import 'package:horus_system/core/domain/value_objects/business_date.dart';
+import 'package:horus_system/features/fleet/presentation/helpers/fleet_license_expiry_update_picker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('fleet license expiry trusted-date bounds', () {
+    final businessDate = BusinessDate(year: 2026, month: 9, day: 19);
+
+    test('derives the lower bound from the supplied business date', () {
+      expect(
+        fleetLicenseExpiryFirstDate(businessDate),
+        DateTime(
+          2026 - AppDateConstraints.fleetLicenseExpiryPastYears,
+          9,
+          19,
+        ),
+      );
+    });
+
+    test('derives the upper bound from the supplied business date', () {
+      expect(
+        fleetLicenseExpiryLastDate(businessDate),
+        DateTime(
+          2026 + AppDateConstraints.fleetLicenseExpiryFutureYears,
+          9,
+          19,
+        ),
+      );
+    });
+  });
+}
