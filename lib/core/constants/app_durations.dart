1f36ac70b7699a3c7763759be84dc7deca09b48f
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-20 21:06:10 +0400

    feat: add network polling duration

--- lib/core/constants/app_durations.dart
+++ lib/core/constants/app_durations.dart
@@ -2,4 +2,5 @@ abstract final class AppDurations {
   static const Duration fast = Duration(milliseconds: 150);
   static const Duration normal = Duration(milliseconds: 250);
   static const Duration slow = Duration(milliseconds: 400);
+  static const Duration networkStatusPollingInterval = Duration(seconds: 3);
 }
