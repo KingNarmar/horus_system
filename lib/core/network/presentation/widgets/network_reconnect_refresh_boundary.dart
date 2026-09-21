6826d0100d13bb93835d2a7b725501afea55f2ba
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-21 14:37:56 +0400

    feat: refresh active workspace after reconnect

---
+++ lib/core/network/presentation/widgets/network_reconnect_refresh_boundary.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/network_status_cubit.dart';
+import '../cubit/network_status_state.dart';
+
+class NetworkReconnectRefreshBoundary extends StatefulWidget {
+  final Widget child;
+
+  const NetworkReconnectRefreshBoundary({
+    required this.child,
+    super.key,
+  });
+
+  @override
+  State<NetworkReconnectRefreshBoundary> createState() =>
+      _NetworkReconnectRefreshBoundaryState();
+}
+
+class _NetworkReconnectRefreshBoundaryState
+    extends State<NetworkReconnectRefreshBoundary> {
+  var _revision = 0;
+  var _refreshPending = false;
+
+  void _handleNetworkState(NetworkStatusState state) {
+    if (state is NetworkStatusOffline || state is NetworkStatusFailure) {
+      _refreshPending = true;
+      return;
+    }
+
+    if (state is NetworkStatusOnline && _refreshPending) {
+      _refreshPending = false;
+      setState(() => _revision += 1);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<NetworkStatusCubit, NetworkStatusState>(
+      listener: (_, state) => _handleNetworkState(state),
+      child: KeyedSubtree(
+        key: ValueKey(_revision),
+        child: widget.child,
+      ),
+    );
+  }
+}
