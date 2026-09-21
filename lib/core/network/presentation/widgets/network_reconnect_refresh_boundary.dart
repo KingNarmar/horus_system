17bd5562956de27ab2be103a0028f72692934115
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-21 14:57:06 +0400

    refactor: rebuild workspace from reconnect revision

--- lib/core/network/presentation/widgets/network_reconnect_refresh_boundary.dart
+++ lib/core/network/presentation/widgets/network_reconnect_refresh_boundary.dart
@@ -4,38 +4,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../cubit/network_status_cubit.dart';
 import '../cubit/network_status_state.dart';
 
-class NetworkReconnectRefreshBoundary extends StatefulWidget {
+class NetworkReconnectRefreshBoundary extends StatelessWidget {
   final Widget child;
 
   const NetworkReconnectRefreshBoundary({required this.child, super.key});
 
   @override
-  State<NetworkReconnectRefreshBoundary> createState() =>
-      _NetworkReconnectRefreshBoundaryState();
-}
-
-class _NetworkReconnectRefreshBoundaryState
-    extends State<NetworkReconnectRefreshBoundary> {
-  var _revision = 0;
-  var _refreshPending = false;
-
-  void _handleNetworkState(NetworkStatusState state) {
-    if (state is NetworkStatusOffline || state is NetworkStatusFailure) {
-      _refreshPending = true;
-      return;
-    }
-
-    if (state is NetworkStatusOnline && _refreshPending) {
-      _refreshPending = false;
-      setState(() => _revision += 1);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocListener<NetworkStatusCubit, NetworkStatusState>(
-      listener: (_, state) => _handleNetworkState(state),
-      child: KeyedSubtree(key: ValueKey(_revision), child: widget.child),
+    return BlocSelector<NetworkStatusCubit, NetworkStatusState, int>(
+      selector: (state) => state.reconnectRevision,
+      builder: (_, reconnectRevision) {
+        return KeyedSubtree(
+          key: ValueKey(reconnectRevision),
+          child: child,
+        );
+      },
     );
   }
 }
