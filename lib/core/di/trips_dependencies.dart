2ee76dd34cf109a68727179feb6eedef27688d61
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-22 18:27:36 +0400

    fix: wire Trip evidence use case

--- lib/core/di/trips_dependencies.dart
+++ lib/core/di/trips_dependencies.dart
@@ -7,6 +7,7 @@ import '../../features/trips/data/datasources/trips_remote_data_source.dart';
 import '../../features/trips/data/repositories/trip_documents_repository_impl.dart';
 import '../../features/trips/data/repositories/trips_repository_impl.dart';
 import '../../features/trips/domain/usecases/get_trip_permissions_usecase.dart';
+import '../../features/trips/domain/usecases/has_required_trip_evidence_usecase.dart';
 import '../../features/trips/domain/usecases/trips_usecases.dart';
 import '../../features/trips/presentation/cubit/trips_cubit.dart';
 import 'business_document_dependencies.dart';
@@ -45,6 +46,8 @@ abstract final class TripsDependencies {
       ),
       getTripStatusHistoryUseCase: GetTripStatusHistoryUseCase(tripsRepository),
       getTripDocumentsUseCase: GetTripDocumentsUseCase(tripDocumentsRepository),
+      hasRequiredTripEvidenceUseCase:
+          const HasRequiredTripEvidenceUseCase(),
       uploadTripDocumentUseCase: UploadTripDocumentUseCase(
         tripDocumentsRepository,
       ),
