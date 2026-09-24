aae87791aa72b6ab9993ba34c1eee661b3e4be8a
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-24 19:53:05 +0400

    refactor: remove legacy client audit coordination

--- lib/features/drivers/data/repositories/drivers_repository_impl.dart
+++ lib/features/drivers/data/repositories/drivers_repository_impl.dart
@@ -5,8 +5,6 @@ import 'package:supabase_flutter/supabase_flutter.dart'
 import '../../../../core/errors/common_failures.dart';
 import '../../../../core/utils/result.dart';
 import '../../../../core/data/utils/uuid_v4.dart';
-import '../../../audit/domain/entities/audit_action.dart';
-import '../../../audit/domain/usecases/create_audit_log_usecase.dart';
 import '../../domain/entities/driver.dart';
 import '../../domain/entities/driver_image_file.dart';
 import '../../domain/entities/driver_image_urls.dart';
@@ -18,20 +16,17 @@ import '../mappers/driver_mapper.dart';
 import '../models/driver_model.dart';
 import 'driver_change_detector.dart';
 import 'driver_image_upload_coordinator.dart';
-import 'driver_repository_audit_writer.dart';
 import 'driver_repository_failure_mapper.dart';
 
 class DriversRepositoryImpl implements DriversRepository {
   final DriversRemoteDataSource remoteDataSource;
   final DriverImagesRemoteDataSource imagesRemoteDataSource;
-  final CreateAuditLogUseCase createAuditLogUseCase;
   final DriverChangeDetector _changeDetector;
   final DriverRepositoryFailureMapper _failureMapper;
 
   const DriversRepositoryImpl({
     required this.remoteDataSource,
     required this.imagesRemoteDataSource,
-    required this.createAuditLogUseCase,
   }) : _changeDetector = const DriverChangeDetector(),
        _failureMapper = const DriverRepositoryFailureMapper();
 
@@ -39,9 +34,6 @@ class DriversRepositoryImpl implements DriversRepository {
     return DriverImageUploadCoordinator(imagesRemoteDataSource);
   }
 
-  DriverRepositoryAuditWriter get _auditWriter {
-    return DriverRepositoryAuditWriter(createAuditLogUseCase);
-  }
 
   @override
   Future<Result<List<Driver>>> getDrivers({required String companyId}) {
@@ -83,12 +75,7 @@ class DriversRepositoryImpl implements DriversRepository {
           driverId: driverId,
           data: dataWithImages,
         );
-        return _withAudit(
-          model: model,
-          actorRole: actorRole,
-          action: AuditAction.created,
-          description: 'driver_created',
-        );
+        return Success(model.toEntity());
       } catch (_) {
         await _imageUploads.removeUploadedImages(paths: uploadedPaths);
         rethrow;
@@ -104,147 +91,9 @@ class DriversRepositoryImpl implements DriversRepository {
     DriverImageUploadSet? imageUploads,
   }) {
     return _guard(() async {
-      final oldModel = await remoteDataSource.getDriverById(
-        companyId: data.companyId,
-        driverId: driverId,
-      );
-      final uploadedPaths = <String>[];
-      try {
-        final dataWithImages = await _imageUploads.dataWithUploadedImages(
-          driverId: driverId,
-          data: data,
-          imageUploads: imageUploads,
-          fallback: oldModel,
-          uploadedPaths: uploadedPaths,
-        );
-        if (!_changeDetector.hasDriverChanges(oldModel, dataWithImages)) {
-          return Success(oldModel.toEntity());
-        }
-        final model = await remoteDataSource.updateDriver(
-          driverId: driverId,
-          data: dataWithImages,
-        );
-        return _withAudit(
-          model: model,
-          actorRole: actorRole,
-          action: AuditAction.updated,
-          description: 'driver_updated',
-          oldValues: oldModel.toAuditValues(),
-        );
-      } catch (_) {
-        await _imageUploads.removeUploadedImages(paths: uploadedPaths);
-        rethrow;
-      }
-    });
-  }
-
-  String _driverIdForInsert() => newUuidV4();
-
-  @override
-  Future<Result<Driver>> deactivateDriver({
-    required String companyId,
-    required String driverId,
-    required String actorRole,
-  }) {
-    return _changeStatus(
-      companyId: companyId,
-      driverId: driverId,
-      actorRole: actorRole,
-      action: AuditAction.deactivated,
-      description: 'driver_deactivated',
-      mutate: remoteDataSource.deactivateDriver,
-    );
-  }
-
-  @override
-  Future<Result<Driver>> reactivateDriver({
-    required String companyId,
-    required String driverId,
-    required String actorRole,
-  }) {
-    return _changeStatus(
-      companyId: companyId,
-      driverId: driverId,
-      actorRole: actorRole,
-      action: AuditAction.reactivated,
-      description: 'driver_reactivated',
-      mutate: remoteDataSource.reactivateDriver,
-    );
-  }
-
-  @override
-  Future<Result<DriverImageUrls>> getDriverImageUrls({required Driver driver}) {
-    return _guard(() async {
-      return Success(
-        DriverImageUrls(
-          profileImageUrl: await _imageUploads.signedUrl(
-            driver.profileImagePath,
-          ),
-          licenseImageUrl: await _imageUploads.signedUrl(
-            driver.licenseImagePath,
-          ),
-          licenseBackImageUrl: await _imageUploads.signedUrl(
-            driver.licenseBackImagePath,
-          ),
-          nationalIdImageUrl: await _imageUploads.signedUrl(
-            driver.nationalIdImagePath,
-          ),
-          nationalIdBackImageUrl: await _imageUploads.signedUrl(
-            driver.nationalIdBackImagePath,
-          ),
-        ),
-      );
-    });
-  }
-
-  Future<Result<Driver>> _changeStatus({
-    required String companyId,
-    required String driverId,
-    required String actorRole,
-    required AuditAction action,
-    required String description,
-    required Future<DriverModel> Function({
-      required String companyId,
-      required String driverId,
-    })
-    mutate,
-  }) {
-    return _guard(() async {
-      final oldModel = await remoteDataSource.getDriverById(
-        companyId: companyId,
-        driverId: driverId,
-      );
       final model = await mutate(companyId: companyId, driverId: driverId);
-      return _withAudit(
-        model: model,
-        actorRole: actorRole,
-        action: action,
-        description: description,
-        oldValues: oldModel.toAuditValues(),
-      );
+      return Success(model.toEntity());
     });
-  }
-
-  Future<Result<Driver>> _withAudit({
-    required DriverModel model,
-    required String actorRole,
-    required AuditAction action,
-    required String description,
-    Map<String, Object?>? oldValues,
-  }) async {
-    final auditFailure = await _auditWriter.writeDriverAudit(
-      companyId: model.companyId,
-      actorRole: actorRole,
-      entityId: model.id,
-      entityDisplayName: model.fullName,
-      action: action,
-      description: description,
-      oldValues: oldValues,
-      newValues: model.toAuditValues(),
-    );
-
-    if (auditFailure != null) return FailureResult(auditFailure);
-    return Success(model.toEntity());
   }
 
   Future<Result<T>> _guard<T>(Future<Result<T>> Function() action) async {
