7c394efe0d7e3e4718045fa02d0ca882646b0aa1
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-24 22:45:00 +0400

    refactor: remove unused audit verification rpc constants

--- lib/features/audit/data/constants/audit_db_fields.dart
+++ lib/features/audit/data/constants/audit_db_fields.dart
@@ -17,16 +17,3 @@ abstract final class AuditDbFields {
   static const newValues = 'new_values';
   static const metadata = 'metadata';
 }
-
-abstract final class AuditDbRpcs {
-  static const assertEventRecorded = 'assert_audit_event_recorded';
-}
-
-abstract final class AuditDbRpcParams {
-  static const companyId = 'p_company_id';
-  static const module = 'p_module';
-  static const entityType = 'p_entity_type';
-  static const entityId = 'p_entity_id';
-  static const action = 'p_action';
-  static const auditEvent = 'p_audit_event';
-}
