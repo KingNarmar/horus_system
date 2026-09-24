a5519ede53bff3cca4f552aa6bb50bdc1fb798a5
Mina A Bushra <mina.bushra1990@gmail.com>
2026-09-24 20:02:46 +0400

    style: format audit hardening refactor

--- lib/core/di/fleet_dependencies.dart
+++ lib/core/di/fleet_dependencies.dart
@@ -18,9 +18,7 @@ abstract final class FleetDependencies {
   static FleetCubit createFleetCubit() {
     final client = SupabaseClientProvider.client;
     final remoteDataSource = SupabaseFleetRemoteDataSource(client);
-    final repository = FleetRepositoryImpl(
-      remoteDataSource: remoteDataSource,
-    );
+    final repository = FleetRepositoryImpl(remoteDataSource: remoteDataSource);
     const businessTimeZoneConverter = TimezoneBusinessTimeZoneConverter();
     return FleetCubit(
       getTractorHeadsUseCase: GetTractorHeadsUseCase(repository),
